11259cacbbdb2b2ef0ffbfb0f36e0dfa8f9d5eb0
StevenL2017 <liang1058@126.com>
2022-09-29 22:12:07 +0800

    update

---
+++ problem/1729D.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 3;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define pq(T)                  priority_queue<T, vector<T>>
+#define rpq(T)                 priority_queue<T, vector<T>, greater<T>>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.begin(), v.end(), greater<auto>())
+
+#define rep(i, n)              for(int i = 0; i < n; ++i)
+#define repA(i, a, n)          for(int i = a; i < n; ++i)
+#define repD(i, a, n)          for(int i = a; i > n; --i)
+#define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    int n;
+    cin >> n;
+    vi x, y;
+    rep(i, n) {
+        int num;
+        cin >> num;
+        x.push_back(num);
+    }
+    rep(i, n) {
+        int num;
+        cin >> num;
+        y.push_back(num);
+    }
+
+    vi diff;
+    rep(i, n) {
+        diff.push_back(y[i] - x[i]);
+    }
+    sortA(diff);
+
+    int ans = 0;
+    int i = 0, j = n - 1;
+    ll pre = diff.front(), suf = diff.back();
+    while (i < j) {
+        if (pre + suf < 0) {
+            i++;
+            pre = diff[i];
+        } else {
+            ans++;
+            i++;
+            j--;
+            pre = diff[i];
+            suf = diff[j];
+        }
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
