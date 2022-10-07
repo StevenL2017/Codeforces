99dcfbbcb2be005811e461b014c117bf9c07d9d5
StevenL2017 <liang1058@126.com>
2022-10-07 21:14:15 +0800

    update

---
+++ problem/1707A.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 3;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define pq(T)                  priority_queue<T, vector<T>>
+#define rpq(T)                 priority_queue<T, vector<T>, greater<T>>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for(int i = 0; i < n; ++i)
+#define repA(i, a, n)          for(int i = a; i < n; ++i)
+#define repD(i, a, n)          for(int i = a; i > n; --i)
+#define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vi a(n);
+    for (auto &x : a) cin >> x;
+
+    vi ans;
+    int cur = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (cur < q) {
+            if (a[i] > cur) {
+                cur++;
+            }
+            ans.push_back(1);
+        } else {
+            if (a[i] > cur) {
+                ans.push_back(0);
+            } else {
+                ans.push_back(1);
+            }
+        }
+    }
+
+    reverse(ans.begin(), ans.end());
+    for (auto &x : ans) {
+        cout << x;
+    }
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
