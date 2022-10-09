2d9648a384400bac3b470914dd70ac1d234147ee
Steven <liang1058@126.com>
2022-10-09 17:25:47 +0800

    update

---
+++ problem/1555C.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 3;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define pq(T)                  priority_queue<T, vector<T>>
+#define rpq(T)                 priority_queue<T, vector<T>, greater<T>>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for(int i = 0; i < n; ++i)
+#define repA(i, a, n)          for(int i = a; i < n; ++i)
+#define repD(i, a, n)          for(int i = a; i > n; --i)
+#define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    int m;
+    cin >> m;
+    vector<vi> mat(2, vi(m));
+    vector<vi> pre(2, vi(m + 1, 0));
+    rep(i, 2) {
+        rep(j, m) {
+            cin >> mat[i][j];
+            pre[i][j + 1] = pre[i][j] + mat[i][j];
+        }
+    }
+
+    int ans = INF;
+    rep(j, m) {
+        ans = min(ans, max(pre[1][j], pre[0][m] - pre[0][j + 1]));
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
