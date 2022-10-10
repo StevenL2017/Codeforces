b7deb0ca537535ecd864f636faa0418c23512c9a
StevenL2017 <liang1058@126.com>
2022-10-10 23:54:33 +0800

    update

---
+++ problem/1736A.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 3;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define pq(T)                  priority_queue<T, vector<T>>
+#define rpq(T)                 priority_queue<T, vector<T>, greater<T>>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for(int i = 0; i < n; ++i)
+#define repA(i, a, n)          for(int i = a; i < n; ++i)
+#define repD(i, a, n)          for(int i = a; i > n; --i)
+#define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n), b(n);
+    int ca = 0, cb = 0, cd = 0;
+    for (auto& x: a) {
+        cin >> x;
+        if (x == 1) {
+            ca++;
+        }
+    }
+    for (auto& x: b) {
+        cin >> x;
+        if (x == 1) {
+            cb++;
+        }
+    }
+    rep(i, n) {
+        if (a[i] != b[i]) {
+            cd++;
+        }
+    }
+
+    if (ca == cb) {
+        if (cd == 0) {
+            cout << 0 << endl;
+        } else {
+            cout << 1 << endl;
+        }
+    } else {
+        if (cd == abs(ca - cb)) {
+            cout << abs(ca - cb) << endl;
+        } else {
+            cout << abs(ca - cb) + 1 << endl;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
