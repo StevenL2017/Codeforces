1978e692f7d68ec72af8ace4b371b5624d21dfbf
StevenL2017 <liang1058@126.com>
2022-10-10 21:50:29 +0800

    update

--- problem/1633D.cpp
+++ problem/1633D.cpp
@@ -31,7 +31,7 @@ typedef pair<int, int> ii;
 
 const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
-const int MAXN = 1e6 + 3;
+const int MAXN = 1e3 + 1;
 
 #define _  %  MOD
 #define __ %= MOD
@@ -47,36 +47,35 @@ const int MAXN = 1e6 + 3;
 #define repD(i, a, n)          for(int i = a; i > n; --i)
 #define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
 
+vi d(MAXN, MAXN);
+
+void calc() {
+    d[1] = 0;
+    for (int i = 1; i < MAXN; i++) {
+        for (int x = 1; x <= i; x++) {
+            int j = i + i / x;
+            if (j < MAXN) d[j] = min(d[j], d[i] + 1);
+        }
+    }
+}
+
 void solve() {
     int n, k;
     cin >> n >> k;
-    vi ops, c(n);
+    vi b(n), c(n);
     int s = 0;
-    rep(i, n) {
-        int b;
-        cin >> b;
-        if (b == 1) {
-            ops.push_back(0);
-        } else {
-            int tot = 0;
-            rep(i, 15) {
-                int cur = 1 << i;
-                if (b & cur) {
-                    tot += (i == 0 ? 1 : i);
-                }
-            }
-            ops.push_back(tot);
-            s += tot;
-        }
+    for (auto& x: b) {
+        cin >> x;
+        s += d[x];
     }
     for (auto& x: c) cin >> x;
-
+    
     k = min(k, s);
     vll f(k + 1, 0);
     rep(i, n) {
         repD(j, k, -1) {
-            if (j - ops[i] >= 0) {
-                f[j] = max(f[j], f[j - ops[i]] + c[i]);
+            if (j - d[b[i]] >= 0) {
+                f[j] = max(f[j], f[j - d[b[i]]] + c[i]);
             }
         }
     }
@@ -86,6 +85,7 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    calc();
     int t = 1;
     cin >> t;
     while (t--) solve();
