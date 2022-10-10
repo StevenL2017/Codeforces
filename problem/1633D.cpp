95e312dc612aec5f80410ae499fd4df41122963d
Steven <liang1058@126.com>
2022-10-10 17:29:53 +0800

    update

---
+++ problem/1633D.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<ll> vll;
+typedef vector<ld> vld;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef pair<int, int> ii;
+
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 3;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define pq(T)                  priority_queue<T, vector<T>>
+#define rpq(T)                 priority_queue<T, vector<T>, greater<T>>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for(int i = 0; i < n; ++i)
+#define repA(i, a, n)          for(int i = a; i < n; ++i)
+#define repD(i, a, n)          for(int i = a; i > n; --i)
+#define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vi ops, c(n);
+    int s = 0;
+    rep(i, n) {
+        int b;
+        cin >> b;
+        if (b == 1) {
+            ops.push_back(0);
+        } else {
+            int tot = 0;
+            rep(i, 15) {
+                int cur = 1 << i;
+                if (b & cur) {
+                    tot += (i == 0 ? 1 : i);
+                }
+            }
+            ops.push_back(tot);
+            s += tot;
+        }
+    }
+    for (auto& x: c) cin >> x;
+
+    k = min(k, s);
+    vll f(k + 1, 0);
+    rep(i, n) {
+        repD(j, k, -1) {
+            if (j - ops[i] >= 0) {
+                f[j] = max(f[j], f[j - ops[i]] + c[i]);
+            }
+        }
+    }
+    cout << f[k] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
