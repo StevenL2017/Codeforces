fb9ea421e888357eeaf23e10c78aba38bad7e1a3
Steven <liang1058@126.com>
2022-10-11 16:14:01 +0800

    update

---
+++ problem/1567C.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+const int MAXN = 1e6 + 3;
+const int INF = 1e9 + 7;
+const int MOD = 1e9 + 7;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+#define loop(i, start, end)    for (auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vi digits;
+    while (n > 0) {
+        digits.push_back(n % 10);
+        n /= 10;
+    }
+
+    reverse(digits.begin(), digits.end());
+    int m = digits.size();
+    int x = 0, y = 0;
+    for (int i = 0; i < m; i += 2) {
+        x *= 10;
+        x += digits[i];
+    }
+    for (int i = 1; i < m; i += 2) {
+        y *= 10;
+        y += digits[i];
+    }
+    cout << (x + 1) * (y + 1) - 2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
