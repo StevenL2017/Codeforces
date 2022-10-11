1bb8f9c9c3ff29086810016d4e32638820578a75
Steven <liang1058@126.com>
2022-10-11 10:15:21 +0800

    update

--- problem/1736C2.cpp
+++ problem/1736C2.cpp
@@ -20,32 +20,22 @@
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
 typedef vector<int> vi;
-typedef vector<double> vd;
-typedef vector<ll> vll;
-typedef vector<ld> vld;
-typedef vector<char> vc;
-typedef vector<string> vs;
-typedef pair<int, int> ii;
 
+const int MAXN = 1e6 + 3;
 const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
-const int MAXN = 1e6 + 3;
 
 #define _  %  MOD
 #define __ %= MOD
 
-#define pq(T)                  priority_queue<T, vector<T>>
-#define rpq(T)                 priority_queue<T, vector<T>, greater<T>>
-
 #define sortA(v)               sort(v.begin(), v.end())
 #define sortD(v)               sort(v.rbegin(), v.rend())
 
-#define rep(i, n)              for(int i = 0; i < n; ++i)
-#define repA(i, a, n)          for(int i = a; i < n; ++i)
-#define repD(i, a, n)          for(int i = a; i > n; --i)
-#define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+#define loop(i, start, end)    for (auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
 
 void solve() {
     int n;
--- problem/1736D.cpp
+++ problem/1736D.cpp
@@ -20,66 +20,31 @@
 using namespace std;
 
 typedef long long ll;
-typedef long double ld;
 typedef vector<int> vi;
-typedef vector<double> vd;
-typedef vector<ll> vll;
-typedef vector<ld> vld;
-typedef vector<char> vc;
-typedef vector<string> vs;
-typedef pair<int, int> ii;
 
+const int MAXN = 1e6 + 3;
 const int INF = 1e9 + 7;
 const int MOD = 1e9 + 7;
-const int MAXN = 1e6 + 3;
 
 #define _  %  MOD
 #define __ %= MOD
 
-#define pq(T)                  priority_queue<T, vector<T>>
-#define rpq(T)                 priority_queue<T, vector<T>, greater<T>>
-
 #define sortA(v)               sort(v.begin(), v.end())
 #define sortD(v)               sort(v.rbegin(), v.rend())
 
-#define rep(i, n)              for(int i = 0; i < n; ++i)
-#define repA(i, a, n)          for(int i = a; i < n; ++i)
-#define repD(i, a, n)          for(int i = a; i > n; --i)
-#define loop(i, start, end)    for(auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+#define loop(i, start, end)    for (auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
 
 void solve() {
     int n;
-    cin >> n;
     string s;
-    cin >> s;
-
-    if (s[0] == '0') {
-        rep(i, 2 * n) {
-            if (s[i] == '0') s[i] = '1';
-            else s[i] = '0';
-        }
-    }
-
-    vi cnt(2, 0);
-    for (auto& c: s) {
-        if (c == '0') {
-            cnt[0]++;
-        } else {
-            cnt[1]++;
-        }
-    }
-    if (cnt[1] % 2 != 0) {
-        cout << -1 << endl;
-        return;
-    }
-
-    
+    cin >> n >> s;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t = 1;
-    cin >> t;
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int t = 1; cin >> t;
     while (t--) solve();
 }
