34daef9d3da077191bca5e761a097f6b1b62622d
Steven <liang1058@126.com>
2022-10-11 10:16:52 +0800

    update

--- problem/1736D.cpp
+++ problem/1736D.cpp
@@ -44,7 +44,9 @@ void solve() {
 }
 
 int main() {
-    ios::sync_with_stdio(false); cin.tie(nullptr);
-    int t = 1; cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
     while (t--) solve();
 }
