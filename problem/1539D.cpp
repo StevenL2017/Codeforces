06ef21e3cc44fa6aea00e5ff25dcd36893f5a3e1
Steven <liang1058@126.com>
2022-10-12 16:05:21 +0800

    update

--- problem/1539D.cpp
+++ problem/1539D.cpp
@@ -38,13 +38,53 @@ const int MOD = 1e9 + 7;
 #define loop(i, start, end)    for (auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
 
 void solve() {
+    int n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    ll tot = 0LL;
+    rep(i, n) {
+        cin >> a[i] >> b[i];
+        tot += a[i];
+    }
 
+    ll ans = 0LL, cnt = 0LL;
+    vi idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&](int i, int j) { return b[i] < b[j]; });
+    int right = n - 1;
+    rep(left, n) {
+        if (cnt < b[idx[left]]) {
+            while (left < right && cnt < b[idx[left]]) {
+                ll d = b[idx[left]] - cnt;
+                if (a[idx[right]] <= d) {
+                    ans += a[idx[right]] * 2;
+                    cnt += a[idx[right]];
+                    right--;
+                } else {
+                    ans += d * 2;
+                    cnt += d;
+                    a[idx[right]] -= d;
+                }
+            }
+            if (cnt < b[idx[left]]) {
+                ll d = b[idx[left]] - cnt;
+                ans += min(d, a[idx[left]]) * 2 + a[idx[left]] - min(d, a[idx[left]]);
+            } else {
+                ans += a[idx[left]];
+            }
+        } else {
+            ans += a[idx[left]];
+        }
+        cnt += a[idx[left]];
+        if (left == right) break;
+    }
+    cout << ans << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) solve();
 }
