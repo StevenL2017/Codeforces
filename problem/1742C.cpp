79d2ba944a73fb442d699b6f0f0648e69ac67084
StevenL2017 <liang1058@126.com>
2022-10-15 22:13:55 +0800

    update

---
+++ problem/1742C.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+const int MAXN = 1e6 + 7;
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7; // const ll INF = 9e18 + 7;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+#define loop(i, start, end)    for (auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    vector<string> mat(8);
+    rep(i, 8) cin >> mat[i];
+
+    rep(r, 8) {
+        int cnt_r = 0;
+        rep(c, 8) {
+            if (mat[r][c] == 'R') cnt_r++;
+        }
+        if (cnt_r == 8) {
+            cout << "R" << endl;
+            return;
+        }
+    }
+
+    rep(c, 8) {
+        int cnt_b = 0;
+        rep(r, 8) {
+            if (mat[r][c] == 'B') cnt_b++;
+        }
+        if (cnt_b == 8) {
+            cout << "B" << endl;
+            return;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
