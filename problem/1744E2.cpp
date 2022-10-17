16e192c99374ef06a923b5e3048093fe5e512af1
Steven <liang1058@126.com>
2022-10-17 11:00:31 +0800

    update

--- problem/1744E2.cpp
+++ problem/1744E2.cpp
@@ -37,9 +37,44 @@ const int INF = 2e9 + 7; // const ll INF = 9e18 + 7;
 #define repD(i, a, n)          for (int i = a; i > n; --i)
 #define loop(i, start, end)    for (auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
 
+vector<int> factor(int num) {
+    vector<int> ans;
+    for (int i = 1; i * i <= num; i++) {
+        if (num % i == 0) {
+            ans.push_back(i);
+            if (num / i != i) {
+                ans.push_back(num / i);
+            }
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
 void solve() {
-    ll a, b, c, d;
+    int a, b, c, d;
     cin >> a >> b >> c >> d;
+
+    vi fa = factor(a);
+    vi fb = factor(b);
+    set<ll> fab;
+    for (auto& x: fa) {
+        for (auto& y: fb) {
+            fab.insert((ll)x * y);
+        }
+    }
+
+    int x = -1, y = -1;
+    for (auto& f: fab) {
+        ll g = 1ll * a * b / f;
+        ll p = f * (a / f + 1);
+        ll q = g * (b / g + 1);
+        if (p <= c && q <= d) {
+            x = p;
+            y = q;
+        }
+    }
+    cout << x << " " << y << endl;
 }
 
 int main() {
