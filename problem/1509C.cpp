8249ed1d571e45d24dfd3ae8cab936c78bf79b0f
Steven <liang1058@126.com>
2022-10-18 11:40:59 +0800

    update

---
+++ problem/1509C.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+const int MAXN = 1e6 + 7;
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7; // const ll INF = 9e18 + 7;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+#define loop(i, start, end)    for (auto i = start; (start<end)?i<end:i>end; (start<end)?i++:i--)
+
+void solve() {
+    int n;
+    cin >> n;
+    vi s(n);
+    rep(i, n) cin >> s[i];
+
+    sortA(s);
+    vector<vector<ll>> f(n, vector<ll>(n, 9e18));
+    rep(i, n) f[i][i] = 0;
+    repD(i, n - 2, -1) {
+        repA(j, i + 1, n) {
+            f[i][j] = s[j] - s[i] + min(f[i + 1][j], f[i][j - 1]);
+        }
+    }
+    cout << f[0][n - 1] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
