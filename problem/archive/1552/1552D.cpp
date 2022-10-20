39e26b57d329bb72749d5fe3b95aa424fca10d66
StevenL2017 <liang1058@126.com>
2022-10-20 21:59:42 +0800

    update

---
+++ problem/archive/1552/1552D.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+
+const int MAXN = 1e6 + 7;
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7; // const ll INF = 9e18 + 7;
+
+#define _  %  MOD
+#define __ %= MOD
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+
+void solve() {
+    int n; cin >> n;
+    vi a(n);
+    rep(i, n) cin >> a[i];
+
+    int tot = 1;
+    rep(i, n) tot *= 3;
+
+    repA(j, 1, tot) {
+        int k = j;
+        int s = 0;
+        rep(i, n) {
+            int flag = k % 3;
+            k /= 3;
+            if (flag == 2) flag = -1;
+            s += a[i] * flag;
+        }
+        if (s == 0) {
+            cout << "YES\n";
+            return;
+        }
+    }
+    cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
