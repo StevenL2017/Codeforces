a54fc73ee8bb81cc3461a2276892b1a5d8248609
StevenL2017 <liang1058@126.com>
2022-10-21 22:29:26 +0800

    update

---
+++ problem/1503A.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+const int MAXN = 1e6 + 7;
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7; // const ll INF = 9e18 + 7;
+
+#define _  %  MOD
+#define __ %= MOD
+#define endl '\n'
+
+#define ll long long
+#define vi vector<int>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+
+void solve() {
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int cnt = 0;
+    rep(i, n) cnt += (s[i] == '1');
+
+    if (s.front() != '1' || s.back() != '1' || cnt & 1) {
+        cout << "NO\n";
+        return;
+    }
+
+    string a, b;
+    int k = 0;
+    bool flip = false;
+    rep(i, n) {
+        if (s[i] == '1') {
+            a += (2 * k < cnt ? '(' : ')');
+            b += a.back();
+            k++;
+        } else {
+            a += (flip ? '(' : ')');
+            b += (flip ? ')' : '(');
+            flip = !flip;
+        }
+    }
+
+    cout << "YES\n" << a << endl << b << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
