77b27b2b3ead199684e1983a48f4a985ab9b5eed
Steven <liang1058@126.com>
2022-10-24 12:31:59 +0800

    update

---
+++ problem/archive/1732/1732A.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+const int MAXN = 1e6 + 7;
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7; // const ll INF = 9e18 + 7;
+
+#define _  %  MOD
+#define __ %= MOD
+#define endl '\n'
+
+#define ll long long
+#define vi vector<int>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+
+void solve() {
+    int n; cin >> n;
+    vi a(n);
+    int g = 0;
+    rep(i, n) {
+        cin >> a[i];
+        g = gcd(g, a[i]);
+    }
+
+    if (g == 1) {
+        cout << 0 << endl;
+    }
+    else if (gcd(g, n) == 1) {
+        cout << 1 << endl;
+    }
+    else if (gcd(g, n - 1) == 1) {
+        cout << 2 << endl;
+    }
+    else {
+        cout << 3 << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
