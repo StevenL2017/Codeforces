49e34e80cf74af05120eb3ba5d8cb969b0b2c6ad
Steven <liang1058@126.com>
2022-10-24 18:25:24 +0800

    update

--- problem/1336A.cpp
+++ problem/1336A.cpp
@@ -47,12 +47,48 @@ void solve() {
         graph[u - 1].push_back(v - 1);
         graph[v - 1].push_back(u - 1);
     }
+
+    vi depth(n, 0);
+    function<void(int, int, int)> dfs1 = [&] (int node, int fa, int d) {
+        depth[node] = d;
+        for (auto& nxt: graph[node]) {
+            if (nxt == fa) continue;
+            dfs1(nxt, node, d + 1);
+        }
+    };
+    dfs1(0, -1, 0);
+
+    vi idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&] (int i, int j) { return depth[i] > depth[j]; });
+
+    vector<bool> indus(n, false);
+    rep(i, k) {
+        indus[idx[i]] = true;
+    }
+
+    function<vi(int, int)> dfs2 = [&] (int node, int fa) -> vi {
+        vi ans(2, 0);
+        for (auto& nxt: graph[node]) {
+            if (nxt == fa) continue;
+            auto temp = dfs2(nxt, node);
+            ans[0] += temp[0];
+            ans[1] += temp[1];
+        }
+        if (indus[node]) {
+            ans[1]++;
+        } else {
+            ans[0] += ans[1];
+        }
+        return ans;
+    };
+    cout << dfs2(0, -1)[0] << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) solve();
 }
