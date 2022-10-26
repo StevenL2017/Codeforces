f0bc11036f3aa69cfa75541b055571341c8b107b
Steven <liang1058@126.com>
2022-10-26 14:57:13 +0800

    update

---
+++ problem/archive/1400/1400D.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <tuple>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <utility>
+
+using namespace std;
+
+const int MAXN = 3e3 + 7;
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7; // const ll INF = 9e18 + 7;
+
+#define _  %  MOD
+#define __ %= MOD
+#define endl '\n'
+
+#define ll long long
+#define vi vector<int>
+
+#define sortA(v)               sort(v.begin(), v.end())
+#define sortD(v)               sort(v.rbegin(), v.rend())
+
+#define rep(i, n)              for (int i = 0; i < n; ++i)
+#define repA(i, a, n)          for (int i = a; i < n; ++i)
+#define repD(i, a, n)          for (int i = a; i > n; --i)
+
+void solve() {
+    int n; cin >> n;
+    vi a(n);
+    rep(i, n) {
+        cin >> a[i];
+        a[i]--;
+    }
+
+    ll ans = 0;
+    vi cnt(n * n, 0);
+    repD(j, n - 3, 0) {
+        int k = j + 1;
+        repA(l, k + 1, n) {
+            cnt[a[k] * n + a[l]]++;
+        }
+        rep(i, j) {
+            ans += cnt[a[i] * n + a[j]];
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
