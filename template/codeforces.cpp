c9c9f001d09db8f9b87dac60c526853838fdacd6
Steven <liang1058@126.com>
2022-10-26 16:45:47 +0800

    update

--- template/codeforces.cpp
+++ template/codeforces.cpp
@@ -20,22 +20,22 @@ using namespace std;
 
 #define ll long long
 #define vi vector<int>
-#define endl '\n'
+
+#define ssz(x)              (int)((x).size())
 #define sorta(v)            sort(v.begin(), v.end())
 #define sortd(v)            sort(v.rbegin(), v.rend())
-#define rev(v)              reverse(v.begin(), v.end())
+
 #define rep(i, n)           for (int i = 0; i < n; ++i)
 #define repa(i, a, n)       for (int i = a; i < n; ++i)
 #define repd(i, a, n)       for (int i = a; i > n; --i)
 
-#define ssz(x) (int)((x).size())
-template <class T> void read(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
-template <class T> void write(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
 
 const int MOD = 1e9 + 7;
-// const int MOD = 998244353;
 const int INF = 2e9 + 7;
-// const ll INF = 9e18 + 7;
+const ll  INF_LL = 9e18 + 7;
 
 void solve() {
 
