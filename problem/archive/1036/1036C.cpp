8986abb85716554e061afce075aa724f47841135
Steven <liang1058@126.com>
2022-10-31 09:46:10 +0800

    update

--- problem/archive/1036/1036C.cpp
+++ problem/archive/1036/1036C.cpp
@@ -38,7 +38,39 @@ const int INF = 2e9 + 7;
 const long long INF_LL = 9e18 + 7;
 
 void solve() {
+    ll l, r; cin >> l >> r;
 
+    auto f = [&] (ll x) -> ll {
+        vi digits;
+        while (x > 0) {
+            digits.push_back(x % 10);
+            x /= 10;
+        }
+        int n = ssz(digits);
+
+        ll memo[20][4][2];
+        memset(memo, -1, sizeof(memo));
+        function<ll(int, int, bool)> dfs = [&] (int pos, int cnt, bool bound) -> ll {
+            if (pos == 0) {
+                return 1;
+            }
+            if (memo[pos][cnt][bound] != -1) {
+                return memo[pos][cnt][bound];
+            }
+
+            ll ans = 0;
+            int up = bound ? digits[pos - 1] : 9;
+            if (cnt == 3) up = 0;
+            rep(i, up + 1) {
+                ans += dfs(pos - 1, cnt + (i == 0 ? 0 : 1), bound && (i == up));
+            }
+            memo[pos][cnt][bound] = ans;
+            return ans;
+        };
+        return dfs(n, 0, true);
+    };
+
+    cout << f(r) - f(l - 1) << endl;
 }
 
 int main() {
