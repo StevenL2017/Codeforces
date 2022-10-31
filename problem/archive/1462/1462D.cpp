805bb8008d3e9828df460bf9178d5f3481a35b32
Steven <liang1058@126.com>
2022-10-31 15:09:32 +0800

    update

---
+++ problem/archive/1462/1462D.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <random>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7;
+const long long INF_LL = 9e18 + 7;
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    auto check = [&] (int x) -> bool {
+        int cur = 0;
+        rep(i, n) {
+            cur += a[i];
+            if (cur > x) return false;
+            if (cur == x) cur = 0;
+        }
+        return cur == 0;
+    };
+
+    int ans = 0, tot = 0;
+    rep(i, n) tot += a[i];
+    rep(i, n) {
+        if (tot % (n - i) == 0 && check(tot / (n - i))) {
+            cout << ans << endl;
+            return;
+        }
+        ans++;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
