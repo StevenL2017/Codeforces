a334105de35541b998e6ab5595846006fb80d92b
Steven <liang1058@126.com>
2022-11-02 14:51:42 +0800

    update

---
+++ problem/archive/1294/1294C.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <random>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7;
+const long long INF_LL = 9e18 + 7;
+
+void solve() {
+    int n; cin >> n;
+    int a = -1, b = -1, c = -1;
+    int x = 2;
+    while (x * x <= n) {
+        if (n % x == 0) {
+            a = x;
+            n /= x;
+            break;
+        }
+        x++;
+    }
+
+    if (a == -1) {
+        cout << "NO" << endl;
+        return;
+    }
+
+    x = a + 1;
+    while (x * x <= n) {
+        if (n % x == 0) {
+            b = x;
+            if (n / x != x && n / x >= 2 && n / x != a) {
+                c = n / x;
+                break;
+            }
+        }
+        x++;
+    }
+
+    if (a >= 2 && b >= 2 && c >= 2) {
+        cout << "YES" << endl << a << " " << b << " " << c << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
---
+++ problem/archive/1328/1328C.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <random>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7;
+const long long INF_LL = 9e18 + 7;
+
+void solve() {
+    int n; string s;
+    cin >> n >> s;
+
+    string a = "1", b = "1";
+    bool flag = false;
+    repa(i, 1, n) {
+        if (s[i] == '0') {
+            a += '0';
+            b += '0';
+        }
+        else if (s[i] == '1') {
+            if (!flag) {
+                a += '1';
+                b += '0';
+                flag = true;
+            } else {
+                a += '0';
+                b += '1';
+            }
+        }
+        else {
+            if (!flag) {
+                a += '1';
+                b += '1';
+            } else {
+                a += '0';
+                b += '2';
+            }
+        }
+    }
+    cout << a << endl << b << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
