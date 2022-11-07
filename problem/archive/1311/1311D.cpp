6c9e9644d396787cc06c432ce8571651ed88e413
StevenL2017 <liang1058@126.com>
2022-11-07 22:13:37 +0800

    update

--- problem/archive/1311/1311D.cpp
+++ problem/archive/1311/1311D.cpp
@@ -38,7 +38,38 @@ const int INF = 2e9 + 7;
 const long long INF_LL = 9e18 + 7;
 
 void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
 
+    int res = INF;
+    int ra = a, rb = b, rc = c;
+    repa(x, 1, 2 * a + 1) {
+        int ca = abs(x - a);
+        for (int y = x; y <= 2 * b; y += x) {
+            int cb = abs(y - b);
+            int cc = INF, tc = c;
+            if (c < y) {
+                cc = abs(y - c);
+                tc = y;
+            }
+            if (c >= y && abs((c / y) * y - c) < cc) {
+                cc = abs((c / y) * y - c);
+                tc = (c / y) * y;
+            }
+            if (c >= y && abs((c / y + 1) * y - c) < cc) {
+                cc = abs((c / y + 1) * y - c);
+                tc = (c / y + 1) * y;
+            }
+
+            if (ca + cb + cc < res) {
+                res = ca + cb + cc;
+                ra = x, rb = y, rc = tc;
+            }
+        }
+    }
+
+    cout << res << endl;
+    cout << ra << " " << rb << " " << rc << endl;
 }
 
 int main() {
