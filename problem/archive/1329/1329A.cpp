9ec2580f8583fadc9ba23840b3c5ece7c7d35714
Steven <liang1058@126.com>
2022-11-07 16:47:48 +0800

    update

---
+++ problem/archive/1329/1329A.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <random>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7;
+const long long INF_LL = 9e18 + 7;
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vi l(m); in(l);
+
+    ll tot = 0;
+    rep(i, m) {
+        tot += l[i];
+    }
+
+    if (tot < n) {
+        cout << -1 << endl;
+        return;
+    }
+
+    vi ans(m);
+    rep(i, m) {
+        if (l[i] > n - i) {
+            cout << -1 << endl;
+            return;
+        }
+        if (tot > n - i) {
+            ans[i] = i + 1;
+        } else {
+            ans[i] = n - tot + 1;
+        }
+        tot -= l[i];
+    }
+
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
