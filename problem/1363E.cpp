7dd30ea7252653da5ee08cafcc0f380963fe95cb
StevenL2017 <liang1058@126.com>
2022-11-08 22:08:26 +0800

    update

--- problem/1363E.cpp
+++ problem/1363E.cpp
@@ -40,7 +40,12 @@ const long long INF_LL = 9e18 + 7;
 void solve() {
     int n; cin >> n;
     vi a(n), b(n), c(n);
-    rep(i, n) cin >> a[i] >> b[i] >> c[i];
+    int cb = 0, cc = 0;
+    rep(i, n) {
+        cin >> a[i] >> b[i] >> c[i];
+        if (b[i]) cb++;
+        if (c[i]) cc++;
+    }
     vector<vi> graph(n);
     rep(i, n - 1) {
         int u, v; cin >> u >> v;
@@ -49,7 +54,46 @@ void solve() {
         graph[v].push_back(u);
     }
 
-    
+    if (cb != cc) {
+        cout << -1 << endl;
+        return;
+    }
+
+    // min cost, cnt of 1 0, cnt of 0 1
+    function<vector<ll>(int, int)> dfs = [&] (int node, int fa) -> vector<ll> {
+        vector<ll> ans(3, 0);
+        ans[0] = INF_LL;
+        ll cost = 0;
+        for (auto& nxt: graph[node]) {
+            if (nxt == fa) continue;
+            auto temp = dfs(nxt, node);
+            cost += temp[0];
+            ans[1] += temp[1];
+            ans[2] += temp[2];
+        }
+
+        if (b[node] == 1 && c[node] == 0 && ans[1] < ans[2]) {
+            ans[0] = min(ans[0], cost + 2 * a[node]);
+        }
+        else if (b[node] == 0 && c[node] == 1 && ans[1] > ans[2]) {
+            ans[0] = min(ans[0], cost + 2 * a[node]);
+        }
+        else {
+            ans[0] = min(ans[0], cost);
+        }
+
+        if (b[node] == 1 && c[node] == 0) {
+            ans[1]++;
+        }
+        else if (b[node] == 0 && c[node] == 1) {
+            ans[2]++;
+        }
+        ans[0] = min(ans[0], min(ans[1], ans[2]) * 2 * a[node]);
+        return ans;
+    };
+
+    auto res = dfs(0, -1);
+    cout << res[0] << endl;
 }
 
 int main() {
