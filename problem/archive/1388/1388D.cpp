91f2926289600bc736df48cecaea80a177a58814
Steven <liang1058@126.com>
2022-11-09 14:16:36 +0800

    update

--- problem/archive/1388/1388D.cpp
+++ problem/archive/1388/1388D.cpp
@@ -38,13 +38,55 @@ const int INF = 2e9 + 7;
 const long long INF_LL = 9e18 + 7;
 
 void solve() {
+    int n; cin >> n;
+    vector<ll> a(n); in(a);
+    vi b(n); in(b);
 
+    vector<vi> graph(n);
+    rep(i, n) {
+        if (b[i] != -1) {
+            graph[b[i] - 1].push_back(i);
+        }
+    }
+
+    ll ans = 0;
+    vector<int> pos, neg, visited(n, 0);
+    function<void(int)> dfs = [&] (int node) {
+        visited[node] = 1;
+        for (auto& nxt: graph[node]) {
+            if (visited[nxt]) continue;
+            dfs(nxt);
+        }
+
+        ans += a[node];
+        if (b[node] != -1 && a[node] > 0) {
+            a[b[node] - 1] += a[node];
+        }
+
+        if (a[node] > 0) pos.push_back(node);
+        else neg.push_back(node);
+    };
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+    reverse(neg.begin(), neg.end());
+
+    cout << ans << endl;
+    for (auto& i: pos) {
+        cout << i + 1 << " ";
+    }
+    for (auto& i: neg) {
+        cout << i + 1 << " ";
+    }
+    cout << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) solve();
 }
