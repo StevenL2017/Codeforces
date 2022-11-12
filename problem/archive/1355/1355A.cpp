8c8b3b0304c8b9e8c8e6cfab391e55639a4923e5
unknown <liang1058@126.com>
2022-11-12 11:03:55 +0800

    update

---
+++ problem/archive/1355/1355A.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <random>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7;
+const long long INF_LL = 9e18 + 7;
+
+void solve() {
+    ll a1, k; cin >> a1 >> k;
+
+    auto get_digits = [&] (ll x) -> vi {
+        vi md(2, 0);
+        md[0] = 9;
+        while (x > 0) {
+            int cur = x % 10;
+            md[0] = min(md[0], cur);
+            md[1] = max(md[1], cur);
+            x /= 10;
+        }
+        return md;
+    };
+
+    repa(i, 1, k) {
+        auto md = get_digits(a1);
+        a1 += 1ll * md[0] * md[1];
+        if (md[0] == 0) break;
+    }
+
+    cout << a1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
