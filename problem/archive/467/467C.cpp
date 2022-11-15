b501e69cf418522294a49413b8b5a32b6fcdb049
Steven <liang1058@126.com>
2022-11-15 11:12:49 +0800

    update

---
+++ problem/archive/467/467C.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <random>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7;
+const long long INF_LL = 9e18 + 7;
+
+void solve() {
+    int n, m, k; cin >> n >> m >> k;
+
+    vector<ll> pre(n + 1, 0);
+    rep(i, n) {
+        ll p; cin >> p;
+        pre[i + 1] = pre[i] + p;
+    }
+
+    vector<vector<ll>> f(k + 1, vector<ll>(n + 1, 0));
+    repa(i, 1, k + 1) {
+        repa(j, 1, n + 1) {
+            if (j >= m) f[i][j] = max(f[i][j - 1], f[i - 1][j - m] + pre[j] - pre[j - m]);
+        }
+    }
+
+    cout << f[k][n] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
