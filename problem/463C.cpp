e787f371f1a0210f1b804d81c69c8474094c4bec
Steven <liang1058@126.com>
2022-11-16 17:29:20 +0800

    update

--- problem/463C.cpp
+++ problem/463C.cpp
@@ -38,7 +38,48 @@ const int INF = 2e9 + 7;
 const long long INF_LL = 9e18 + 7;
 
 void solve() {
+    int n; cin >> n;
+    vector<vector<ll>> mat(n, vector<ll>(n));
+    rep(i, n) in(mat[i]);
 
+    map<int, ll> left, right;
+    rep(i, n) {
+        rep(j, n) {
+            left[i + j] += mat[i][j];
+            right[i - j] += mat[i][j];
+        }
+    }
+    
+    vector<vector<ll>> val(2, vector<ll>(2, -INF_LL));
+    vector<vector<pair<int, int>>> idx(2, vector<pair<int, int>>(2));
+    rep(i, n) {
+        rep(j, n) {
+            auto v = left[i + j] + right[i - j] - mat[i][j];
+            if (v > val[(i + j) % 2][abs(i - j) % 2]) {
+                val[(i + j) % 2][abs(i - j) % 2] = v;
+                idx[(i + j) % 2][abs(i - j) % 2] = {i + 1, j + 1};
+            }
+        }
+    }
+
+    ll mx = -INF_LL;
+    int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+    rep(i, 2) {
+        rep(j, 2) {
+            auto l1 = i, r1 = j, l2 = i ^ 1, r2 = j ^ 1;
+            auto v = val[l1][r1] + val[l2][r2];
+            if (v > mx) {
+                mx = v;
+                x1 = idx[l1][r1].first;
+                y1 = idx[l1][r1].second;
+                x2 = idx[l2][r2].first;
+                y2 = idx[l2][r2].second;
+            }
+        }
+    }
+
+    cout << mx << endl;
+    cout << x1 << " " << y1 << " " << x2 << " " << y2 << endl;
 }
 
 int main() {
