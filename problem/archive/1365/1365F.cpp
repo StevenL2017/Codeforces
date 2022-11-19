9db91da91141fe48bec458dd02a018eb31848d07
StevenL2017 <liang1058@126.com>
2022-11-19 22:43:54 +0800

    update

---
+++ problem/archive/1365/1365F.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <random>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int INF = 2e9 + 7;
+const long long INF_LL = 9e18 + 7;
+
+void solve() {
+    int n; cin >> n;
+    vi a(n), b(n); in(a), in(b);
+
+    if ((n & 1) && (a[n / 2] != b[n / 2])) {
+        cout << "NO\n";
+        return;
+    }
+
+    vi used(n / 2, 0);
+    rep(i, n / 2) {
+        auto x = b[i], y = b[n - 1 - i];
+        bool found = false;
+        rep(j, n / 2) {
+            if (used[j]) continue;
+            if (a[j] == x && a[n - 1 - j] == y || a[j] == y && a[n - 1 - j] == x) {
+                used[j] = 1;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
