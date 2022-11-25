ae939cae33d6e225f79574f6bea86b9aedc8e893
unknown <liang1058@126.com>
2022-11-25 16:27:44 +0800

    update

--- problem/archive/484/484B.cpp
+++ problem/archive/484/484B.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdio>
 #include <cmath>
 #include <cstring>
 #include <string>
@@ -8,6 +7,8 @@
 #include <stack>
 #include <queue>
 #include <deque>
+#include <unordered_set>
+#include <unordered_map>
 #include <set>
 #include <map>
 #include <bitset>
@@ -15,7 +16,6 @@
 #include <algorithm>
 #include <functional>
 #include <random>
-
 using namespace std;
 
 #define ll long long
@@ -25,9 +25,9 @@ using namespace std;
 #define sorta(v)            sort(v.begin(), v.end())
 #define sortd(v)            sort(v.rbegin(), v.rend())
 
-#define rep(i, n)           for (int i = 0; i < n; i++)
-#define repa(i, a, n)       for (int i = a; i < n; i++)
-#define repd(i, a, n)       for (int i = a; i > n; i--)
+#define rep(i, n)           for (int i = 0; i < n; ++i)
+#define repa(i, a, n)       for (int i = a; i < n; ++i)
+#define repd(i, a, n)       for (int i = a; i > n; --i)
 
 #define endl '\n'
 template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
@@ -38,13 +38,37 @@ const int INF = 2e9 + 7;
 const long long INF_LL = 9e18 + 7;
 
 void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
 
+    sorta(a);
+    auto mx = a.back() * 2;
+    vi b(mx + 1, -1);
+    int j = 0;
+    rep(i, mx + 1) {
+        while (j < n && a[j] < i) {
+            j++;
+        }
+        if (j > 0) {
+            b[i] = a[j - 1];
+        }
+    }
+
+    int ans = 0;
+    rep(i, n) {
+        if (i > 0 && a[i] == a[i - 1]) continue;
+        for (int j = 2 * a[i]; j <= mx; j += a[i]) {
+            ans = max(ans, b[j] % a[i]);
+        }
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) solve();
 }
