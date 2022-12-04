e10b97e552fc0b42ca0e201a4667a570cd9f7700
StevenL2017 <liang1058@126.com>
2022-12-04 10:06:42 +0800

    update

--- problem/archive/1393/1393D.cpp
+++ problem/archive/1393/1393D.cpp
@@ -40,39 +40,23 @@ const long long INF_LL = 9e18 + 7;
 void solve() {
     int n, m; cin >> n >> m;
     vector<string> mat(n); in(mat);
-    
-    vector<vector<vi>> pre(n, vector<vi>(m, vi(8, 1)));
-    repa(i, 1, n) {
-        rep(j, m) {
-            if (j > 0 && mat[i][j] == mat[i - 1][j - 1]) pre[i][j][0] = pre[i - 1][j - 1][0] + 1;
-            if (mat[i][j] == mat[i - 1][j]) pre[i][j][1] = pre[i - 1][j][1] + 1;
-        }
-    }
-    rep(i, n) {
-        repd(j, m - 2, -1) {
-            if (i > 0 && mat[i][j] == mat[i - 1][j + 1]) pre[i][j][2] = pre[i - 1][j + 1][2] + 1;
-            if (mat[i][j] == mat[i][j + 1]) pre[i][j][3] = pre[i][j + 1][3] + 1;
-        }
-    }
-    repd(i, n - 2, -1) {
-        repd(j, m - 1, -1) {
-            if (j < m - 1 && mat[i][j] == mat[i + 1][j + 1]) pre[i][j][4] = pre[i + 1][j + 1][4] + 1;
-            if (mat[i][j] == mat[i + 1][j]) pre[i][j][5] = pre[i + 1][j][5] + 1;
-        }
-    }
-    repd(i, n - 1, -1) {
-        repa(j, 1, m) {
-            if (i < n - 1 && mat[i][j] == mat[i + 1][j - 1]) pre[i][j][6] = pre[i + 1][j - 1][6] + 1;
-            if (mat[i][j] == mat[i][j - 1]) pre[i][j][7] = pre[i][j - 1][7] + 1;
+
+    vector<vector<int>> f(n, vector<int>(m, 1));
+    repa(i, 2, n) {
+        repa(j, 1, m - 1) {
+            if (mat[i][j] == mat[i - 1][j - 1] && 
+                mat[i][j] == mat[i - 1][j] && 
+                mat[i][j] == mat[i - 1][j + 1] && 
+                mat[i][j] == mat[i - 2][j]) {
+                f[i][j] = min({f[i - 1][j - 1], f[i - 1][j], f[i - 1][j + 1], f[i - 2][j]}) + 1;
+            }
         }
     }
 
     ll ans = 0;
     rep(i, n) {
         rep(j, m) {
-            auto mn1 = min({pre[i][j][1], pre[i][j][3], pre[i][j][5], pre[i][j][7]});
-            auto mn2 = min({pre[i][j][0], pre[i][j][2], pre[i][j][4], pre[i][j][6]});
-            ans += min(mn1, mn2 * 2);
+            ans += f[i][j];
         }
     }
 
