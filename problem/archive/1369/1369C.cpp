4ec8a7ee754e12d535e54373e48f3f0bd041c72d
unknown <liang1058@126.com>
2022-12-07 16:09:22 +0800

    update

---
+++ problem/archive/1369/1369C.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi a(n); in(a);
+
+    sortd(a);
+    ll ans = 0;
+    int i = 0, j = n - 1;
+    vi w;
+    rep(z, k) {
+        int x; cin >> x;
+        if (x == 1) {
+            ans += 2ll * a[i];
+            i++;
+        } else {
+            w.push_back(x);
+        }
+    }
+
+    sortd(w);
+    for (auto& x: w) {
+        ans += a[i] + a[j];
+        i++;
+        j -= x - 1;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
