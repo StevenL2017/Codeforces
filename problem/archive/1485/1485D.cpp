917533dea4c6703a75e29a94b66e1b228c178b23
StevenL2017 <liang1058@126.com>
2022-12-10 20:28:38 +0800

    update

---
+++ problem/archive/1485/1485D.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<vi> mat(n, vi(m));
+    rep(i, n) in(mat[i]);
+
+    vector<vi> ans(n, vi(m, 720720));
+    rep(i, n) {
+        rep(j, m) {
+            if ((i + j) & 1) {
+                ans[i][j] += mat[i][j] * mat[i][j] * mat[i][j] * mat[i][j];
+            }
+        }
+    }
+    rep(i, n) out(ans[i]);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
