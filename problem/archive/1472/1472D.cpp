76ddf8dcb3c5170f39c781b663366a31ece354a5
StevenL2017 <liang1058@126.com>
2022-12-19 10:04:28 +0800

    update

---
+++ problem/archive/1472/1472D.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    sortd(a);
+    ll ca = 0, cb = 0;
+    int i = 0;
+    while (i < n) {
+        if (a[i] % 2 == 0) {
+            ca += a[i];
+        }
+        i++;
+        if (i == n) break;
+        if (a[i] % 2 == 1) {
+            cb += a[i];
+        }
+        i++;
+    }
+
+    if (ca > cb) cout << "Alice\n";
+    else if (ca < cb) cout << "Bob\n";
+    else cout << "Tie\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
