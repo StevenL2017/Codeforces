d2a41bb2c950bb4b96c2d1f13155e1f5a052ff8c
StevenL2017 <liang1058@126.com>
2022-12-24 19:48:22 +0800

    update

---
+++ problem/archive/1110/1110E.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi c(n), t(n); in(c), in(t);
+
+    if (c[0] != t[0]) {
+        cout << "No\n";
+        return;
+    }
+
+    vi cd, td;
+    rep(i, n - 1) {
+        cd.push_back(c[i + 1] - c[i]);
+        td.push_back(t[i + 1] - t[i]);
+    }
+
+    sorta(cd);
+    sorta(td);
+    rep(i, n - 1) {
+        if (cd[i] != td[i]) {
+            cout << "No\n";
+            return;
+        }
+    }
+    cout << "Yes\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
