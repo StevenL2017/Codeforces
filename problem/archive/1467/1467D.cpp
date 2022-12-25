70d607bd785fb7f28e3753f398c5cd412e91e9f5
StevenL2017 <liang1058@126.com>
2022-12-25 20:12:36 +0800

    update

---
+++ problem/archive/1467/1467D.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int n, k, q;
+    cin >> n >> k >> q;
+    vector<ll> a(n); in(a);
+
+    vector<vector<ll>> f(n, vector<ll>(k + 1, 0));
+    rep(i, n) f[i][0] = 1;
+    repa(j, 1, k + 1) {
+        f[0][j] = f[1][j - 1];
+        repa(i, 1, n - 1) {
+            f[i][j] = (f[i - 1][j - 1] + f[i + 1][j - 1]) % MOD;
+        }
+        f[n - 1][j] = f[n - 2][j - 1];
+    }
+
+    vector<ll> cnt(n, 0);
+    rep(i, n) {
+        rep(j, k + 1) {
+            cnt[i] += f[i][j] * f[i][k - j];
+            cnt[i] %= MOD;
+        }
+    }
+
+    ll ans = 0;
+    rep(i, n) {
+        ans += a[i] * cnt[i];
+        ans %= MOD;
+    }
+
+    while (q--) {
+        int i, x;
+        cin >> i >> x;
+        i--;
+
+        ans -= (a[i] * cnt[i]) % MOD;
+        ans += MOD;
+        ans %= MOD;
+        a[i] = x;
+        ans += a[i] * cnt[i];
+        ans %= MOD;
+
+        cout << ans << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
