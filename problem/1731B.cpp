dbb73a8a145c2b6b4ffa8695e3a854920d88bc22
StevenL2017 <liang1058@126.com>
2022-12-28 00:35:49 +0800

    update

---
+++ problem/1731B.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}   
+
+int sub(int x, int y) {
+    return add(x, -y);
+}   
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y) {
+    int z = 1;
+    while (y)
+    {
+        if (y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+int inv(int x) {
+	return binpow(x, MOD - 2);
+}
+
+int divide(int x, int y) {
+	return mul(x, inv(y));
+}
+
+void solve() {
+    int n; cin >> n;
+
+    ll ans = 0;
+    ans += divide(mul(mul(n, n + 1), 2 * n + 1), 6);
+    ans %= MOD;
+    n--;
+    ans += divide(mul(mul(n, n + 1), 2 * n + 1), 6);
+    ans %= MOD;
+    ans += divide(mul(n, n + 1), 2);
+    ans %= MOD;
+
+    cout << (ans * 2022ll) % MOD << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
