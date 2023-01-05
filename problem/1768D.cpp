e89540c1ab7df975787a8f1f42932fd81a4e2525
StevenL2017 <liang1058@126.com>
2023-01-06 00:37:16 +0800

    update

---
+++ problem/1768D.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+class UnionFind {
+public:
+    int N;
+    int *f;
+    int *sz;
+    int count;
+
+public:
+    UnionFind(int n) :
+        N(n), count(n) {
+        f = new int[N]();
+        sz = new int[N]();
+        for (int i = 0; i < N; i++) {
+            f[i] = i;
+            sz[i] = 1;
+        }
+    }
+    
+    ~UnionFind() {
+        delete[] f;
+        delete[] sz;
+    }
+
+    int find(int x) {
+        if (x != f[x]) f[x] = find(f[x]);
+        return f[x];
+    }
+
+    bool merge(int x, int y) {
+        int x_ = find(x), y_ = find(y);
+        if (x_ == y_) return false;
+        if (sz[x_] < sz[y_]) swap(x_, y_);
+        f[y_] = x_;
+        sz[x_] += sz[y_];
+        count--;
+        return true;
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+void solve() {
+    int n; cin >> n;
+    vi p(n); in(p);
+
+    UnionFind uf(n);
+    rep(i, n) {
+        uf.merge(i, p[i] - 1);
+    }
+
+    bool adj = false;
+    vi a(n, -10);
+    rep(i, n) {
+        if (a[uf.find(i)] + 1 == i) adj = true;
+        a[uf.find(i)] = i;
+    }
+
+    int cnt = 0;
+    vi vis(n, 0);
+    rep(i, n) {
+        if (vis[uf.find(i)]) continue;
+        cnt += uf.sz[uf.find(i)] - 1;
+        vis[uf.find(i)] = 1;
+    }
+
+    int ans = (adj && cnt > 0) ? cnt - 1 : cnt + 1;
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
