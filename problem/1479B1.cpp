dbd59b21feb597ddd8738d3e209d12e6e9d5da22
Steven <liang1058@126.com>
2023-01-06 17:26:45 +0800

    update

--- problem/1479B1.cpp
+++ problem/1479B1.cpp
@@ -34,13 +34,42 @@ template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
 template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
 
 void solve() {
+    int n; cin >> n;
+    vi a(n + 1, 0); rep(i, n) cin >> a[i + 1];
 
+    vi pos(n + 1, n + 1), nxt(n + 1, 0);
+    repd(i, n, -1) {
+        nxt[i] = pos[a[i]];
+        pos[a[i]] = i;
+    }
+    
+    int ans = 0, x = 0, y = 0;
+    repa(i, 1, n + 1) {
+        if (a[x] == a[i]) {
+            ans += a[y] != a[i];
+            y = i;
+        }
+        else if (a[y] == a[i]) {
+            ans += a[x] != a[i];
+            x = i;
+        }
+        else if (nxt[x] < nxt[y]) {
+            ans += a[x] != a[i];
+            x = i;
+        }
+        else {
+            ans += a[y] != a[i];
+            y = i;
+        }
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while (t--) solve();
 }
