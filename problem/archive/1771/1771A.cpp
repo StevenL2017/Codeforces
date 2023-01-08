f75493459f566dd10c1bfb65abb67461a435cd68
StevenL2017 <liang1058@126.com>
2023-01-08 19:40:08 +0800

    update

---
+++ problem/archive/1771/1771A.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll n; cin >> n;
+    vi a(n); in(a);
+
+    sorta(a);
+    if (a.front() == a.back()) {
+        cout << n * (n - 1) << endl;
+    } else {
+        ll c1 = 0, c2 = 0;
+        rep(i, n) {
+            if (a[i] == a.front()) c1++;
+            if (a[i] == a.back()) c2++;
+        }
+        cout << 2 * c1 * c2 << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
