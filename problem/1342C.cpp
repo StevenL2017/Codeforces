43ea86cf2081c72967c71d09272a8f3e14da35a9
Steven <liang1058@126.com>
2023-01-09 17:34:41 +0800

    update

---
+++ problem/1342C.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll a, b, q;
+    cin >> a >> b >> q;
+
+    if (a > b) swap(a, b);
+
+    vector<ll> ans(q, 0);
+    rep(i, q) {
+        ll l, r; cin >> l >> r;
+        if (b % a != 0 && r >= b) {
+            l = max(l, b);
+            auto cnt = max(0ll, (r - l)) / (a + b);
+            ans[i] = max(0ll, r - l + 1 - cnt);
+        }
+    }
+
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
