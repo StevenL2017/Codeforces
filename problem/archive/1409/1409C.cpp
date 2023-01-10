418be70b01cd5ddff33e9bf3f3eb3549a4ff7676
Steven <liang1058@126.com>
2023-01-10 15:50:57 +0800

    update

---
+++ problem/archive/1409/1409C.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, x, y;
+    cin >> n >> x >> y;
+
+    int d = y - x;
+    repa(i, (y - x + n - 2) / (n - 1), y - x) {
+        if ((y - x) % i == 0) {
+            d = i;
+            break;
+        }
+    }
+
+    int m = n - ((y - x) / d + 1), a0 = x;
+    while (m > 0 && a0 - d > 0) {
+        m--;
+        a0 -= d;
+    }
+
+    vi ans;
+    rep(i, n) {
+        ans.push_back(a0);
+        a0 += d;
+    }
+
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
