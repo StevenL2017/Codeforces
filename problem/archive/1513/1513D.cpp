5acacb75c79fc4035715ba1672ca70da1abf306c
StevenL2017 <liang1058@126.com>
2023-01-16 21:46:01 +0800

    update

---
+++ problem/archive/1513/1513D.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, p; cin >> n >> p;
+    vi a(n); in(a);
+
+    vi idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&] (int i, int j) { return a[i] < a[j]; });
+
+    vi weight(n - 1, p);
+    for (auto& i: idx) {
+        int s = i, e = i;
+        while (s > 0 && a[s - 1] % a[i] == 0 && weight[s - 1] > a[i]) {
+            s--;
+            weight[s] = a[i];
+        }
+        while (e < n - 1 && a[e + 1] % a[i] == 0 && weight[e] > a[i]) {
+            weight[e] = a[i];
+            e++;
+        }
+    }
+
+    ll ans = 0;
+    for (auto& w: weight) {
+        ans += w;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
