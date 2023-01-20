a171339be170e4464a8140daaac2491e2e086e4a
StevenL2017 <liang1058@126.com>
2023-01-20 11:46:20 +0800

    update

---
+++ problem/1503C.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vector<pair<int, int>> ac(n);
+    rep(i, n) cin >> ac[i].first >> ac[i].second;
+
+    ll ans = 0;
+    rep(i, n) ans += ac[i].second;
+
+    sorta(ac);
+    int mx = ac.front().first + ac.front().second;
+    repa(i, 1, n) {
+        ans += max(0, ac[i].first - mx);
+        mx = max(mx, ac[i].first + ac[i].second);
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
