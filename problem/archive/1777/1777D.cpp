d0e1caca3ce34baa1cc797d5d7633a178aff25f6
StevenL2017 <liang1058@126.com>
2023-01-22 21:02:02 +0800

    update

--- problem/archive/1777/1777D.cpp
+++ problem/archive/1777/1777D.cpp
@@ -34,6 +34,33 @@ template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
 template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
 
 const int MOD = 1e9 + 7;
+const int MAXN = 2e5 + 3;
+
+int pow2[MAXN];
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int fastexp(int b, int exp) {
+    if (exp == 0) return 1;
+    int temp = fastexp(b, exp / 2);
+    temp = mul(temp, temp);
+    if (exp % 2 == 1) temp = mul(temp, b);
+    return temp;
+}
+
+void precompute(int n) {
+    for (int i = 0; i <= n; i++)
+        pow2[i] = fastexp(2, i);
+}
 
 void solve() {
     int n; cin >> n;
@@ -44,11 +71,26 @@ void solve() {
         e[u].push_back(v);
         e[v].push_back(u);
     }
+
+    int ans = 0;
+    function<int(int, int, int)> dfs = [&] (int u, int p, int d) -> int {
+        int cur = d;
+        for (auto& v: e[u]) {
+            if (v == p) continue;
+            cur = max(cur, dfs(v, u, d + 1));
+        }
+        ans = add(ans, cur - d + 1);
+        return cur;
+    };
+    dfs(0, -1, 0);
+
+    cout << mul(ans, pow2[n - 1]) << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    precompute(MAXN - 1);
     int t = 1;
     cin >> t;
     while (t--) solve();
