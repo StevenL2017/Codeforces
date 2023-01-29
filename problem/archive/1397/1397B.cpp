263a1f82c73b6d51e60493ad0ba7f0fc488ddbc7
Steven <liang1058@126.com>
2023-01-29 16:55:27 +0800

    update

---
+++ problem/archive/1397/1397B.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    sorta(a);
+    ll ans = accumulate(a.begin(), a.end(), 0ll) - n;
+    bool ok = true;
+    for (int x = 2; x < 1e5; x++) {
+        ll cur = 0, p = 1;
+        rep(i, n) {
+            cur += abs(a[i] - p);
+            if (p > 1e18) {
+                ok = false;
+                break;
+            }
+            p *= x;
+        }
+        if (!ok) break;
+        ans = min(ans, cur);
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
---
+++ problem/archive/1433/1433E.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MAXN = 21;
+
+ll factorial[MAXN];
+
+void get_factorial() {
+    factorial[0] = 1;
+    for (int i = 1; i < MAXN; i++) factorial[i] = i * factorial[i - 1];
+}
+
+void solve() {
+    int n; cin >> n;
+
+    get_factorial();
+    cout << (factorial[n] / factorial[n / 2] / factorial[n / 2] / 2) * (factorial[n / 2] / (n / 2)) * (factorial[n / 2] / (n / 2)) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
