df1bc381e60238ca813fc5f30108c3618316dc75
Steven <liang1058@126.com>
2023-02-06 08:57:24 +0800

    update

---
+++ problem/archive/1167/1167C.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+class UnionFind {
+public:
+    int N;
+    int *f;
+    int *sz;
+    int count;
+
+public:
+    UnionFind(int n) :
+        N(n), count(n) {
+        f = new int[N]();
+        sz = new int[N]();
+        for (int i = 0; i < N; i++) {
+            f[i] = i;
+            sz[i] = 1;
+        }
+    }
+    
+    ~UnionFind() {
+        delete[] f;
+        delete[] sz;
+    }
+
+    int find(int x) {
+        if (x != f[x]) f[x] = find(f[x]);
+        return f[x];
+    }
+
+    bool merge(int x, int y) {
+        int x_ = find(x), y_ = find(y);
+        if (x_ == y_) return false;
+        if (sz[x_] < sz[y_]) swap(x_, y_);
+        f[y_] = x_;
+        sz[x_] += sz[y_];
+        count--;
+        return true;
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+void solve() {
+    int n, m; cin >> n >> m;
+    UnionFind uf(n);
+    rep(i, m) {
+        int k; cin >> k;
+        vi a(k); in(a);
+        repa(i, 1, k) {
+            uf.merge(a[0] - 1, a[i] - 1);
+        }
+    }
+
+    vi ans(n);
+    rep(i, n) {
+        ans[i] = uf.sz[uf.find(i)];
+    }
+    
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
