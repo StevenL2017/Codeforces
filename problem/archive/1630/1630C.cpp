a020af424a39e9af3d34a41ae2b1d3fc6cf6c0d0
Steven <liang1058@126.com>
2023-02-08 11:24:38 +0800

    update

---
+++ problem/archive/1630/1630C.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+class SegmentTree {
+private:
+    vector<int> mx;
+
+public:
+    SegmentTree(int n) {
+        mx.resize(n * 4, -2e9);
+    }
+    
+    void update(int root, int l, int r, int idx, int val) {
+        if (l == r) {
+            mx[root] = val;
+            return;
+        }
+        int m = l + (r - l) / 2;
+        if (idx <= m) {
+            update(root * 2, l, m, idx, val);
+        } else {
+            update(root * 2 + 1, m + 1, r, idx, val);
+        }
+        mx[root] = max(mx[root * 2], mx[root * 2 + 1]);
+    }
+    
+    int query(int root, int l, int r, int L, int R) {
+        if (L <= l && r <= R) {
+            return mx[root];
+        }
+        int ans = -2e9;
+        int m = l + (r - l) / 2;
+        if (L <= m) {
+            ans = query(root * 2, l, m, L, R);
+        }
+        if (R > m) {
+            ans = max(ans, query(root * 2 + 1, m + 1, r, L, R));
+        }
+        return ans;
+    }
+};
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    vi first(n + 1, n + 1);
+    repd(i, n - 1, -1) {
+        first[a[i]] = i;
+    }
+
+    SegmentTree tree(n);
+    tree.update(1, 1, n, 1, 0);
+    repa(i, 1, n) {
+        int left = min(first[a[i]], i - 1);
+        tree.update(1, 1, n, i + 1, tree.query(1, 1, n, left + 1, i) - 1);
+    }
+
+    cout << tree.query(1, 1, n, n, n) + n - 1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
