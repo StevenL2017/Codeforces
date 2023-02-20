012f3e70b017fbf613e69675762d44201cec5c24
StevenL2017 <liang1058@126.com>
2023-02-20 22:02:43 +0800

    update

---
+++ problem/1234B1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vi a(n); in(a);
+
+    deque<int> q;
+    set<int> s;
+    rep(i, n) {
+        if (!s.count(a[i])) {
+            if (ssz(q) == k) {
+                auto f = q.front();
+                q.pop_front();
+                s.erase(f);
+            }
+            q.push_back(a[i]);
+            s.insert(a[i]);
+        }
+    }
+
+    vi ans;
+    while (!q.empty()) {
+        auto b = q.back();
+        q.pop_back();
+        ans.push_back(b);
+    }
+
+    cout << ssz(ans) << endl;
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
