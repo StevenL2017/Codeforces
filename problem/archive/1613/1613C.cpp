96534ba0360d50891015ccfd1af41d7acf13ccdf
StevenL2017 <liang1058@126.com>
2023-02-20 22:10:50 +0800

    update

---
+++ problem/archive/1613/1613C.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; ll h;
+    cin >> n >> h;
+    vi a(n); in(a);
+
+    auto check = [&] (ll x) -> bool {
+        ll cnt = x;
+        rep(i, n - 1) {
+            cnt += min(x, (a[i + 1] - a[i]) * 1ll);
+        }
+        return cnt >= h;
+    };
+
+    ll left = 1, right = h, ans = right;
+    while (left <= right) {
+        auto mid = left + (right - left) / 2;
+        if (check(mid)) {
+            right = mid - 1;
+            ans = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
