fdd386032911686c2db4e4e8b409bc9e1d715b37
StevenL2017 <liang1058@126.com>
2023-03-12 20:34:21 +0800

    update

---
+++ problem/archive/977/977D.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vector<ll> a(n); in(a);
+
+    map<int, vector<ll>> mp;
+    for (auto& x: a) {
+        ll y = x, cnt = 0;
+        while (y % 3 == 0) {
+            y /= 3;
+            cnt++;
+        }
+        mp[cnt].push_back(x);
+    }
+    
+    vector<ll> ans;
+    for (auto& [k, v]: mp) {
+        sortd(v);
+        for (auto& x: v) {
+            ans.push_back(x);
+        }
+    }
+    reverse(ans.begin(), ans.end());
+
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
