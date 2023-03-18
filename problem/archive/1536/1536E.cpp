e49d467f1ce99e3ec6b5aac1c58d09b9b04fcd4b
StevenL2017 <liang1058@126.com>
2023-03-18 21:56:37 +0800

    update

---
+++ problem/archive/1536/1536E.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<string> a(n); in(a);
+
+    bool has_zero = false;
+    int ans = 1;
+    rep(i, n) {
+        rep(j, m) {
+            if (a[i][j] == '0') has_zero = true;
+            else ans = (ans * 1ll * 2) % MOD;
+        }
+    }
+    if (!has_zero) ans = (ans - 1 + MOD) % MOD;
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
