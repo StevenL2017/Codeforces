cb738225edbe3298a0ee4fa89cf13db6e1193314
Steven <liang1058@126.com>
2023-03-23 14:21:57 +0800

    update

---
+++ problem/archive/1730/1730D.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; string s1, s2;
+    cin >> n >> s1 >> s2;
+
+    map<pair<char, char>, int> cnt;
+    rep(i, n) {
+        auto f = min(s1[i], s2[n - 1 - i]);
+        auto s = max(s1[i], s2[n - 1 - i]);
+        cnt[{f, s}]++;
+    }
+
+    int odd = 0;
+    for (auto& [k, v]: cnt) {
+        if (v & 1) {
+            if (k.first != k.second) {
+                cout << "NO\n";
+                return;
+            }
+            odd++;
+            if (odd > 1) {
+                cout << "NO\n";
+                return;
+            }
+        }
+    }
+    cout << "YES\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
