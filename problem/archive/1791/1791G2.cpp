0cda56f23258702d07c3f8a77038f8685633401a
StevenL2017 <liang1058@126.com>
2023-03-26 20:59:53 +0800

    update

--- problem/archive/1791/1791G2.cpp
+++ problem/archive/1791/1791G2.cpp
@@ -37,14 +37,40 @@ void solve() {
     int n, c; cin >> n >> c;
     vi a(n); in(a);
 
-    vi left(n), right(n), b(n);
+    vector<pair<int, int>> b(n);
     rep(i, n) {
-        left[i] = a[i] + i + 1;
-        right[i] = a[i] + n - i;
-        b[i] = min(left[i], right[i]);
+        b[i] = {min(a[i] + i + 1, a[i] + n - i), a[i] + i + 1};
+    }
+    sorta(b);
+
+    vector<ll> pre(n + 1, 0);
+    rep(i, n) {
+        pre[i + 1] = pre[i] + b[i].first;
     }
 
-    
+    int ans = 0;
+    rep(i, n) {
+        int nc = c - b[i].second;
+        int left = 0, right = n, mx = left;
+        while (left <= right) {
+            auto mid = left + (right - left) / 2;
+            auto cost = pre[mid];
+            auto cur = mid + 1;
+            if (mid > i) {
+                cost -= b[i].first;
+                cur--;
+            }
+            if (cost <= nc) {
+                left = mid + 1;
+                mx = max(mx, cur);
+            } else {
+                right = mid - 1;
+            }
+        }
+        ans = max(ans, mx);
+    }
+
+    cout << ans << endl;
 }
 
 int main() {
