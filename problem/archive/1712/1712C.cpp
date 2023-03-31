061f37e2c17bec6ccdf62ef8da7b17aebedb2f34
Steven <liang1058@126.com>
2023-03-31 08:58:51 +0800

    update

---
+++ problem/archive/1712/1712C.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    int mx = *max_element(a.begin(), a.end());
+    vi last(mx + 1, -1);
+    rep(i, n) {
+        last[a[i]] = i;
+    }
+
+    int end = 0;
+    rep(i, n - 1) {
+        if (a[i] > a[i + 1]) {
+            end = i + 1;
+        }
+    }
+
+    set<int> s;
+    rep(i, end) {
+        if (a[i]) {
+            s.insert(a[i]);
+            a[last[a[i]]] = 0;
+        }
+    }
+
+    end = 0;
+    rep(i, n) {
+        if (!a[i]) {
+            end = i + 1;
+        }
+    }
+
+    rep(i, end) {
+        if (a[i]) {
+            s.insert(a[i]);
+        }
+    }
+
+    cout << ssz(s) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
