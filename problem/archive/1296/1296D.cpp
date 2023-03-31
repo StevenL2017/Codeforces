fb395a8430f73816da5edf1d3bfacd507a0bfeb6
Steven <liang1058@126.com>
2023-03-31 13:43:05 +0800

    update

---
+++ problem/archive/1296/1296D.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, a, b, k;
+    cin >> n >> a >> b >> k;
+    vi h(n); in(h);
+
+    vector<ll> cnt(n, 0);
+    rep(i, n) {
+        int x = h[i] % (a + b);
+        if (x == 0) {
+            x = a + b;
+        }
+        cnt[i] = (x - 1) / a;
+    }
+
+    sorta(cnt);
+    repa(i, 1, n) {
+        cnt[i] += cnt[i - 1];
+    }
+
+    int ans = upper_bound(cnt.begin(), cnt.end(), k) - cnt.begin();
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
