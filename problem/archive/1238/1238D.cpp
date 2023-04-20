71704204e1e1c9afbfb6cc13ea330569858d7744
Steven <liang1058@126.com>
2023-04-20 10:56:41 +0800

    update

---
+++ problem/archive/1238/1238D.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; string s;
+    cin >> n >> s;
+
+    vi sub;
+    for (int i = 0, j = 0; i < n; ) {
+        while (j < n && s[j] == s[i]) {
+            j++;
+        }
+        sub.push_back(j - i);
+        i = j;
+    }
+
+    ll invalid = 0;
+    int cnt = ssz(sub);
+    rep(i, cnt - 1) {
+        invalid += sub[i] + sub[i + 1];
+    }
+    invalid -= (cnt - 1);
+
+    cout << n * 1ll * (n + 1) / 2 - invalid - n << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
