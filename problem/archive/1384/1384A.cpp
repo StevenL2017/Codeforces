cd14f51f1b281c1dc4cd42fef770f8bba2b9fc2e
StevenL2017 <liang1058@126.com>
2023-05-01 22:12:29 +0800

    update

---
+++ problem/archive/1384/1384A.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    int mx = *max_element(a.begin(), a.end());
+
+    vector<string> ans(n + 1, string(mx + 1, 'a'));
+    rep(i, n) {
+        rep(j, 26) {
+            if (j != ans[i][a[i]] - 'a') {
+                char c = j + 'a';
+                ans[i + 1] = ans[i].substr(0, a[i]) + string(mx + 1 - a[i], c);
+                break;
+            }
+        }
+    }
+
+    for (auto& c: ans) {
+        cout << c << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
