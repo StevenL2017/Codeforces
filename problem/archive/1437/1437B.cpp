9c18f4d59ee2c6e7b15856dd4050103188951b5f
StevenL2017 <liang1058@126.com>
2023-05-03 11:04:40 +0800

    update

---
+++ problem/archive/1437/1437B.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+
+#define ssz(x)              (int)((x).size())
+#define sorta(v)            sort(v.begin(), v.end())
+#define sortd(v)            sort(v.rbegin(), v.rend())
+
+#define rep(i, n)           for (int i = 0; i < n; i++)
+#define repa(i, a, n)       for (int i = a; i < n; i++)
+#define repd(i, a, n)       for (int i = a; i > n; i--)
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { rep(i, ssz(a)) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { rep(i, ssz(a)) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+
+    int ans0 = 0, ans1 = 0;
+    for (int i = 0, j = 0; i < n; ) {
+        while (j < n && s[j] == s[i]) {
+            j++;
+        }
+        if (s[i] == '0') {
+            ans0 += j - i - 1;
+        } else {
+            ans1 += j - i - 1;
+        }
+        i = j;
+    }
+
+    cout << max(ans0, ans1) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
