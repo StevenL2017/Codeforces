5da659137e95454a049fa4531dd9981b512cacee
StevenL2017 <liang1058@126.com>
2023-05-15 00:24:20 +0800

    update

---
+++ problem/1827A.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+void solve() {
+    int n; cin >> n;
+    vi a(n), b(n);
+    in(a); in(b);
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int ans = 1;
+    int i = n - 1, j = n - 1, cnt = 0;
+    while (j >= 0) {
+        while (i >= 0 && j >= 0 && a[i] > b[j]) {
+            cnt++;
+            i--;
+        }
+        if (cnt == 0) {
+            cout << 0 << endl;
+            return;
+        }
+        ans = mul(ans, cnt);
+        cnt--;
+        j--;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
