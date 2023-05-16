59894318cae7c0eeb8df9c962a3ab8cf78fe9799
Steven <liang1058@126.com>
2023-05-16 10:24:40 +0800

    update

---
+++ problem/archive/903/903D.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+class BIT {
+public:
+    int minv, maxv, N;
+    long long all;
+    long long *tr;
+
+    // 给定定义域 [minv, maxv], 初始化一个树状数组
+    BIT(int minv, int maxv) :
+        minv(minv), maxv(maxv), N(maxv - minv + 2), all(0) {
+        if (minv > maxv) {
+            cout << "BIT Init: Value Range Invalid!" << endl;
+        } else {
+            tr = new long long[N]();
+        }
+    }
+
+    ~BIT() {
+        delete[] tr;
+    }
+
+    void add(int x, long long v) {
+        if (x < minv || x > maxv) {
+            cout << "Add: Index Invalid!" << endl;
+        } else {
+            all += v;
+            x = x - minv + 1;
+            while (x < N) {
+                tr[x] += v;
+                x += x & -x;
+            }
+        }
+    }
+
+    // 求小于等于 x 的值的和
+    // 如果求小于，那么用 query_LE(x - 1)
+    long long query_LE(int x) {
+        x = max(0, min(x - minv + 1, N - 1));
+        long long ret = 0;
+        while (x) {
+            ret += tr[x];
+            x -= x & -x;
+        }
+        return ret;
+    }
+
+    // 求大于等于 x 的值的和
+    // 如果求大于，那么用 query_BE(x + 1)
+    long long query_BE(int x) {
+        return all - query_LE(x - 1);
+    }
+};
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    vi b = a;
+    sort(b.begin(), b.end());
+    b.erase(unique(b.begin(), b.end()), b.end());
+    int x = 0;
+    map<int, int> mp;
+    mp[b[0]] = x;
+    for (int i = 1; i < ssz(b); i++) {
+        if (b[i] - b[i - 1] == 1) {
+            x++;
+        } else {
+            x += 2;
+        }
+        mp[b[i]] = x;
+    }
+
+    long double ans = 0.0;
+    BIT tree(0, x), cnt(0, x);
+    for (int i = 0; i < n; i++) {
+        int cur = mp[a[i]];
+        ans += cnt.query_LE(cur - 2) * a[i] - tree.query_LE(cur - 2);
+        ans += cnt.query_BE(cur + 2) * a[i] - tree.query_BE(cur + 2);
+        tree.add(cur, a[i]);
+        cnt.add(cur, 1);
+    }
+
+    cout.precision(20);
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
