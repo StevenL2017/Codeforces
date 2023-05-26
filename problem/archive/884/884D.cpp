d342641aa05d9aead48d52c88b86232e435eb5e6
Steven <liang1058@126.com>
2023-05-26 11:38:28 +0800

    update

---
+++ problem/archive/884/884D.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    if (n % 2 == 0) a.push_back(0);
+    priority_queue<ll, vector<ll>, greater<ll>> q;
+    for (auto x : a) q.emplace(x);
+
+    ll ans = 0;
+    while (ssz(q) > 1) {
+        ll cur = 0;
+        for (int i = 0; i < 3; i++) {
+            cur += q.top();
+            q.pop();
+        }
+        ans += cur;
+        q.emplace(cur);
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
