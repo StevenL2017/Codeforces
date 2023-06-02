a3ccbed9df1f985644a8f4075830c26c3364a6cf
Steven <liang1058@126.com>
2023-06-02 10:17:17 +0800

    update

---
+++ problem/archive/1566/1566E.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vector<vi> e(n);
+    for (int i = 0; i < n - 1; i++) {
+        int u, v; cin >> u >> v;
+        u--; v--;
+        e[u].push_back(v);
+        e[v].push_back(u);
+    }
+
+    int ans = 0, root_leaf = 0;
+    function<int(int, int)> dfs = [&] (int u, int p) -> int {
+        int is_bud = 0;
+        for (auto v : e[u]) {
+            if (v == p) continue;
+            is_bud |= dfs(v, u);
+        }
+        if (is_bud) {
+            if (u != 0) ans++;
+            else root_leaf = 1;
+        }
+        return 1 - is_bud;
+    };
+    dfs(0, -1);
+
+    cout << n - 2 * ans - root_leaf << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
