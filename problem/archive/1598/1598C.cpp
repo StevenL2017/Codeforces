7c3960c71d5c07ced45406bcfae80c359d22cb13
StevenL2017 <liang1058@126.com>
2023-06-03 06:51:56 +0800

    update

---
+++ problem/archive/1598/1598C.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    ll tot = 0;
+    for (int i = 0; i < n; i++) {
+        tot += a[i];
+    }
+
+    if (2 * tot % n != 0) {
+        cout << 0 << endl;
+        return;
+    }
+
+    ll ans = 0, target = 2 * tot / n;
+    map<int, int> cnt;
+    for (auto x : a) {
+        ans += cnt[target - x];
+        cnt[x]++;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
