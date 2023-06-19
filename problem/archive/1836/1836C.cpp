b4d5f993ccdd9ed1abcf80d599951b25116c5818
Steven <liang1058@126.com>
2023-06-19 08:55:18 +0800

    update

--- problem/archive/1836/1836C.cpp
+++ problem/archive/1836/1836C.cpp
@@ -27,7 +27,27 @@ template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin 
 template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
 
 void solve() {
+    int a, b, c; ll k;
+    cin >> a >> b >> c >> k;
 
+    int x_mn = pow(10, a - 1), x_mx = pow(10, a) - 1;
+    int y_mn = pow(10, b - 1), y_mx = pow(10, b) - 1;
+    int z_mn = pow(10, c - 1), z_mx = pow(10, c) - 1;
+
+    for (int x = x_mn; x <= x_mx; x++) {
+        int l = max(x + y_mn, z_mn), r = min(x + y_mx, z_mx);
+        if (l > r) continue;
+        int cnt = r - l + 1;
+        if (k > cnt) {
+            k -= cnt;
+        } else {
+            int y = l - x + k - 1;
+            cout << x << " + " << y << " = " << x + y << endl;
+            return;
+        }
+    }
+
+    cout << -1 << endl;
 }
 
 int main() {
