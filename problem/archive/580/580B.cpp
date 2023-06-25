4d7030ba30bd96caa0bf51bbfa906019e095d031
Steven <liang1058@126.com>
2023-06-25 09:07:02 +0800

    update

---
+++ problem/archive/580/580B.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, d; cin >> n >> d;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first >> a[i].second;
+    }
+
+    sort(a.begin(), a.end());
+
+    ll ans = 0, tot = 0;
+    for (int right = 0, left = 0; right < n; right++) {
+        tot += a[right].second;
+        while (left < right && a[right].first - a[left].first >= d) {
+            tot -= a[left].second;
+            left++;
+        }
+        ans = max(ans, tot);
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
