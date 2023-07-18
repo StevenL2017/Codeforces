efb5f5889553344e5d5e50f0006053cf64b7d2b9
Steven <liang1058@126.com>
2023-07-18 11:10:15 +0800

    update

---
+++ problem/archive/1497/1497B.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vi a(n); in(a);
+
+    vi cnt(m, 0);
+    for (auto x : a) {
+        cnt[x % m]++;
+    }
+
+    int ans = 0;
+    for (int i = 0; i <= m / 2; i++) {
+        int j = (m - i) % m;
+        if (i == j) {
+            if (cnt[i]) ans++;
+            continue;
+        }
+        int x = cnt[i], y = cnt[j];
+        if (x < y) swap(x, y);
+        if (y) x = max(0, x - y - 1) + 1;
+        ans += x;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
---
+++ problem/archive/1703/1703F.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    ll ans = 0;
+    vi cnt(n, 0);
+    for (int i = 0; i < n; i++) {
+        if (i > 0 && a[i] < i + 1 && a[i] - 2 >= 0) ans += cnt[min(i - 1, a[i] - 2)];
+        if (i > 0) cnt[i] += cnt[i - 1];
+        if (a[i] < i + 1) cnt[i]++;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
