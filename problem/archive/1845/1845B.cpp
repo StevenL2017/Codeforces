39c7c2798e128082dd4f1e39fb88e0d481cea47b
StevenL2017 <liang1058@126.com>
2023-08-01 21:45:05 +0800

    update

---
+++ problem/archive/1845/1845B.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int x1, y1; cin >> x1 >> y1;
+    int x2, y2; cin >> x2 >> y2;
+    int x3, y3; cin >> x3 >> y3;
+
+    int ans = 1;
+    if (x2 >= x1 && x3 >= x1 || x2 <= x1 && x3 <= x1) {
+        ans += min(abs(x2 - x1), abs(x3 - x1));
+    }
+    if (y2 >= y1 && y3 >= y1 || y2 <= y1 && y3 <= y1) {
+        ans += min(abs(y2 - y1), abs(y3 - y1));
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
