95ebdba508c6768691d205d7907671e70a207f0c
mac <>
2023-08-28 09:23:50 +0800

    update

---
+++ problem/archive/1864/1864E.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 998244353;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y) {
+    int z = 1;
+    while (y) {
+        if (y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+int inv(int x) {
+    return binpow(x, MOD - 2);
+}
+
+int divide(int x, int y) {
+    return mul(x, inv(y));
+}
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    int ans = mul(n, n);
+    sort(a.begin(), a.end());
+    for (int k = 0; k < 30; k++) {
+        for (int i = 0, j = 0; i < n; i++) {
+            int c1 = 0;
+            while (j < n && a[i] / 2 == a[j] / 2) {
+                if (a[j] & 1) c1++;
+                j++;
+            }
+            ans = add(ans, mul(c1, j - i));
+        }
+        for (auto& x: a) {
+            x >>= 1;
+        }
+    }
+
+    cout << divide(ans, mul(n, n)) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
