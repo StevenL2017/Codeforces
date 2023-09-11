92bc6357783774d0202e5b5b368396b84c74f193
mac <>
2023-09-11 14:01:56 +0800

    update

---
+++ problem/archive/300/300C.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+const int MAXN = 1e6 + 3;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y) {
+    int z = 1;
+    while (y) {
+        if (y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+int inv(int x) {
+    return binpow(x, MOD - 2);
+}
+
+int divide(int x, int y) {
+    return mul(x, inv(y));
+}
+
+int factorial[MAXN];
+
+void get_factorial() {
+    factorial[0] = 1;
+    for (int i = 1; i < MAXN; i++) factorial[i] = mul(i, factorial[i - 1]);
+}
+
+int comb(int n, int k) {
+    if (k > n) return 0;
+    return divide(factorial[n], mul(factorial[n - k], factorial[k]));
+}
+
+void solve() {
+    get_factorial();
+
+    int a, b, n;
+    cin >> a >> b >> n;
+
+    auto check = [&] (int x) -> bool {
+        while (x) {
+            int m = x % 10;
+            if (m != a && m != b) {
+                return false;
+            }
+            x /= 10;
+        }
+        return true;
+    };
+
+    int ans = 0, tot = a * n;
+    for (int i = 0; i <= n; i++) {
+        if (check(tot)) {
+            ans = add(ans, comb(n, i));
+        }
+        tot += b - a;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
