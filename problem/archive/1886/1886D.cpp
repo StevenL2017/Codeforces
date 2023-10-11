a756813fa448d7e916d625f86eadf79b5e53b55a
mac <>
2023-10-11 10:38:40 +0800

    update

---
+++ problem/archive/1886/1886D.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 998244353;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y) {
+    int z = 1;
+    while (y) {
+        if (y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+int inv(int x) {
+    return binpow(x, MOD - 2);
+}
+
+int divide(int x, int y) {
+    return mul(x, inv(y));
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+
+    int ans = 1;
+    for (int i = n - 2; i > 0; i--) {
+        if (s[i] == '?') {
+            ans = mul(ans, i);
+        }
+    }
+    cout << (s[0] == '?' ? 0 : ans) << endl;
+
+    while (m--) {
+        int i; cin >> i;
+        char c; cin >> c;
+        i--;
+        if (i && c == '?' && s[i] != '?') {
+            ans = mul(ans, i);
+        }
+        if (i && c != '?' && s[i] == '?') {
+            ans = divide(ans, i);
+        }
+        s[i] = c;
+        cout << (s[0] == '?' ? 0 : ans) << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
