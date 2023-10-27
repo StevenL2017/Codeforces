a839e88e371a2cdccff8c616404b360485ee178f
mac <>
2023-10-27 12:55:12 +0800

    update

---
+++ problem/archive/268/268C.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    int k = min(n, m) + 1;
+    cout << k << endl;
+    for (int i = 0; i < k; i++) {
+        if (n < m) cout << i << " " << i + 1 << endl;
+        else if (n > m) cout << i + 1 << " " << i << endl;
+        else cout << n - i << " " << i << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
