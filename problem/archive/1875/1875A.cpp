b0014d99ec06928fa6fa1e60ec7a96d66558fc52
StevenL2017 <liang1058@126.com>
2023-11-11 17:54:41 +0800

    update

---
+++ problem/archive/1875/1875A.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int a, b, n;
+    cin >> a >> b >> n;
+    vi x(n); in(x);
+
+    ll ans = b - 1;
+    for (auto y: x) {
+        ans += min(y + 1, a) - 1;
+    }
+    cout << ans + 1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
