4c1d8633b8742e24314d0d2e566bea21e0364824
mac <>
2023-11-20 15:41:35 +0800

    update

---
+++ problem/archive/1883/1883B.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+
+    vi cnt(26);
+    for (auto c: s) {
+        cnt[c - 'a']++;
+    }
+
+    int odd = 0;
+    for (auto x: cnt) {
+        if (x & 1) {
+            odd++;
+        }
+    }
+
+    if (odd > 1) {
+        k -= odd - 1;
+    }
+
+    if (k < 0) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
