5e606aaad1fa8b2a8113bf577dce6a30a5d93487
mac <>
2023-12-06 17:50:36 +0800

    update

---
+++ problem/242C.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    pair<int, int> start, end;
+    cin >> start.first >> start.second;
+    cin >> end.first >> end.second;
+    int n; cin >> n;
+    vector<array<int, 3>> a(n, array<int, 3>());
+    for (int i = 0; i < n; i++) {
+        cin >> a[i][0] >> a[i][1] >> a[i][2];
+    }
+
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
