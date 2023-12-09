bb4849c4545e5fe4108729b3314fbeddbd3409d9
StevenL2017 <liang1058@126.com>
2023-12-09 17:35:21 +0800

    update

---
+++ problem/archive/1900/1900B.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    vi ans(3, 0);
+    if (abs(b - c) % 2 == 0) ans[0] = 1;
+    if (abs(a - c) % 2 == 0) ans[1] = 1;
+    if (abs(a - b) % 2 == 0) ans[2] = 1;
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
