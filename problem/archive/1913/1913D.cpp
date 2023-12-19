ab683d405e0412858c93cea4cb7f5712655f290a
StevenL2017 <liang1058@126.com>
2023-12-19 23:21:32 +0800

    update

--- problem/archive/1913/1913D.cpp
+++ problem/archive/1913/1913D.cpp
@@ -28,50 +28,25 @@ template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i+
 
 const int MOD = 998244353;
 
-int add(int x, int y) {
-    x += y;
-    while (x >= MOD) x -= MOD;
-    while (x < 0) x += MOD;
-    return x;
-}
-
-int sub(int x, int y) {
-    return add(x, -y);
-}
-
-int mul(int x, int y) {
-    return (x * 1ll * y) % MOD;
-}
-
 void solve() {
     int n; cin >> n;
     vi a(n); in(a);
 
-    vi right(n, -1);
-    vi st;
-    for (int i = n - 1; i >= 0; i--) {
-        while (!st.empty() && a[st.back()] > a[i]) {
-            st.pop_back();
+    vi f(n), pre(n + 1);
+    stack<int> st;
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        while (!st.empty() && a[st.top()] > a[i]) {
+            ans -= f[st.top()];
+            st.pop();
         }
-        if (!st.empty()) right[i] = st.back();
-        st.push_back(i);
+        int j = st.empty() ? 0 : st.top() + 1;
+        f[i] = (ans + pre[i] - pre[j] + st.empty()) % MOD;
+        pre[i + 1] = (pre[i] + f[i]) % MOD;
+        ans = (ans + f[i]) % MOD;
+        st.push(i);
     }
-
-    vector<ll> f(n + 1, 0);
-    int idx = n - 1, mn = a.back();
-    for (int i = n - 1; i >= 0; i--) {
-        if (right[i] == -1) {
-            f[i] = add(f[i + 1], add(f[idx + 1], 1));
-        } else {
-            f[i] = add(f[i + 1], f[right[i]]);
-        }
-        if (a[i] < mn) {
-            mn = a[i];
-            idx = i;
-        }
-    }
-
-    cout << f[0] << endl;
+    cout << (ans + MOD) % MOD << endl;
 }
 
 int main() {
