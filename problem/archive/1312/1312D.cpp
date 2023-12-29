ae173d1b127a185db012340f890610ec8604de40
mac <>
2023-12-29 16:40:48 +0800

    update

---
+++ problem/archive/1312/1312D.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 998244353;
+const int MAXN = 2e5 + 3;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y) {
+    int z = 1;
+    while (y) {
+        if (y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+int inv(int x) {
+    return binpow(x, MOD - 2);
+}
+
+int divide(int x, int y) {
+    return mul(x, inv(y));
+}
+
+int factorial[MAXN];
+
+void get_factorial() {
+    factorial[0] = 1;
+    for (int i = 1; i < MAXN; i++) factorial[i] = mul(i, factorial[i - 1]);
+}
+
+int comb(int n, int k) {
+    if (k > n) return 0;
+    return divide(factorial[n], mul(factorial[n - k], factorial[k]));
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    if (n == 2) {
+        cout << 0 << endl;
+        return;
+    }
+
+    get_factorial();
+
+    cout << mul(mul(comb(m, n - 1), n - 2), binpow(2, n - 3)) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
