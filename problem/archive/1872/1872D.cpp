c6dc146b0ecab030b21c4299acdf10d5b79bb2a0
StevenL2017 <liang1058@126.com>
2024-01-11 00:02:17 +0800

    update

---
+++ problem/archive/1872/1872D.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll n, x, y;
+    cin >> n >> x >> y;
+
+    ll cxy = n / lcm(x, y);
+    ll cx = n / x - cxy, cy = n / y - cxy;
+    cout << (n - cx + 1 + n) * cx / 2 - (1 + cy) * cy / 2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
