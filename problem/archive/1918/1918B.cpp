7e664ea5f38b944904e008b0bdbc5b6712ded0cb
StevenL2017 <liang1058@126.com>
2024-02-09 22:03:06 +0800

    update

---
+++ problem/archive/1918/1918B.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n), b(n);
+    in(a); in(b);
+
+    vi idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&] (int i, int j) { return a[i] < a[j]; });
+
+    for (int i = 0; i < n; i++) {
+        cout << a[idx[i]] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++) {
+        cout << b[idx[i]] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
