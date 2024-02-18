e0b6a1e719180404acb111a72c991abe18a11d47
mac <>
2024-02-18 09:27:24 +0800

    update

---
+++ problem/archive/1927/1927B.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    string s;
+    vi cnt(26);
+    for (auto x: a) {
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] == x) {
+                s += (char)(i + 'a');
+                cnt[i]++;
+                break;
+            }
+        }
+    }
+    cout << s << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
