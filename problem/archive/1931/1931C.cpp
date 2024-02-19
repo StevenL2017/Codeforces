63078cc7a456b4642b69175420110022217d97f9
mac <>
2024-02-19 13:01:56 +0800

    update

---
+++ problem/archive/1931/1931C.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    int i = 0, j = n - 1;
+    while (i + 1 < n && a[i + 1] == a[i]) {
+        i++;
+    }
+    while (j - 1 >= 0 && a[j - 1] == a[j]) {
+        j--;
+    }
+
+    if (a[0] == a[n - 1]) {
+        cout << max(0, j - i - 1) << endl;
+    } else {
+        cout << min(n - i - 1, j) << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
