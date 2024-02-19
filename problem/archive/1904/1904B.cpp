9c0d0c6cf60c491642217cee087cac03f07bd89e
unknown <liang1058@126.com>
2024-02-20 00:25:29 +0800

    update

---
+++ problem/archive/1904/1904B.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    vi idx(n);
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&] (int i, int j) { return a[i] < a[j]; });
+
+    vi ans(n);
+    ll pre = 0;
+    for (int i = 0; i < n; ) {
+        int i0 = i;
+        pre += a[idx[i++]];
+        while (i < n && pre >= a[idx[i]]) {
+            pre += a[idx[i++]];
+        }
+        for (int j = i0; j < i; j++) {
+            ans[idx[j]] = i - 1;
+        }
+    }
+
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
