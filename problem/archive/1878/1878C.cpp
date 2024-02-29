f692b243de5f42dc8f83934278f31ba12920aed0
mac <>
2024-02-29 12:51:59 +0800

    update

---
+++ problem/archive/1878/1878C.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    ll x;
+    cin >> x;
+
+    ll mn = (1 + k) * 1ll * k / 2;
+    ll mx = (n + n - k + 1) * 1ll * k / 2;
+    if (mn <= x && x <= mx) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
