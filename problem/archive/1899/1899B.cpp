853b96f1969ce208268cd7498552f511ae7c090a
unknown <liang1058@126.com>
2024-03-02 22:09:06 +0800

    update

---
+++ problem/archive/1899/1899B.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    in(a);
+
+    ll ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (n % i != 0) continue;
+        ll mn = LLONG_MAX, mx = LLONG_MIN;
+        for (int j = 0; j < n; j += i) {
+            ll s = 0;
+            for (int k = j; k < j + i; k++) {
+                s += a[k];
+            }
+            mn = min(mn, s);
+            mx = max(mx, s);
+        }
+        ans = max(ans, mx - mn);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
