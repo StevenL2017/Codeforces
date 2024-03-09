0ff59007f7085c26349b9033d9f2836b85f5173b
unknown <liang1058@126.com>
2024-03-09 20:32:16 +0800

    update

---
+++ problem/archive/1209/1209D.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+class UnionFind {
+public:
+    int N;
+    int *f;
+    int *sz;
+    int count;
+    
+    UnionFind(int n) :
+        N(n), count(n) {
+        f = new int[N]();
+        sz = new int[N]();
+        for (int i = 0; i < N; i++) {
+            f[i] = i;
+            sz[i] = 1;
+        }
+    }
+    
+    ~UnionFind() {
+        delete[] f;
+        delete[] sz;
+    }
+
+    int find(int x) {
+        if (x != f[x]) f[x] = find(f[x]);
+        return f[x];
+    }
+
+    bool merge(int x, int y) {
+        int x_ = find(x), y_ = find(y);
+        if (x_ == y_) return false;
+        if (sz[x_] < sz[y_]) swap(x_, y_);
+        f[y_] = x_;
+        sz[x_] += sz[y_];
+        count--;
+        return true;
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    UnionFind uf(n);
+    for (int i = 0; i < k; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        uf.merge(u, v);
+    }
+    cout << k - (n - uf.count) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
