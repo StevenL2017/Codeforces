0356ec4b1b4495f3c1233b9a022801bbe7a4bc88
StevenL2017 <liang1058@126.com>
2024-03-21 00:22:13 +0800

    update

---
+++ problem/archive/1772/1772D.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    int mn = 0, mx = 1e9;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            int x = (a[i] + a[i + 1] + 1) / 2;
+            mn = max(mn, x);
+        } else if (a[i] < a[i + 1]) {
+            int x = (a[i] + a[i + 1]) / 2;
+            mx = min(mx, x);
+        }
+    }
+
+    if (mn > mx) {
+        cout << -1 << endl;
+        return;
+    }
+    cout << mn << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
