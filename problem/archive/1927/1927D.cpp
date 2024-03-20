ee8bba61411093f26c6565453cbe7002905dd6c3
StevenL2017 <liang1058@126.com>
2024-03-20 23:20:29 +0800

    update

---
+++ problem/archive/1927/1927D.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    vi nxt(n);
+    for (int i = n - 1, j = n; i >= 0; i--) {
+        if (i == n - 1 || a[i] != a[i + 1]) {
+            j = i + 1;
+        }
+        nxt[i] = j;
+    }
+
+    int q; cin >> q;
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        l--; r--;
+
+        if (nxt[l] > r) {
+            cout << -1 << " " << -1 << endl;
+        } else {
+            cout << l + 1 << " " << nxt[l] + 1 << endl;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
