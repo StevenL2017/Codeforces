78498f27dc63187b455293cb582f1e6da0ba45c1
unknown <liang1058@126.com>
2024-03-24 14:19:56 +0800

    update

---
+++ problem/archive/287/287B.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+
+    ll d = k * (k - 1) - 2 * (n - 1);
+    if (d < 0) {
+        cout << -1 << endl;
+        return;
+    }
+
+    ll t = int(sqrt(d));
+    cout << k - 1 - t + (t * t + t > d) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
