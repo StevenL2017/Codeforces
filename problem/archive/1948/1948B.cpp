daead5704d7622d1f30515b2b6c7f6c126a4727c
unknown <liang1058@126.com>
2024-03-30 16:45:43 +0800

    update

---
+++ problem/archive/1948/1948B.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n; cin >> n;
+    vi a(n); in(a);
+
+    vi b({a[n - 1]});
+    for (int i = n - 2; i >= 0; i--) {
+        if (a[i] > b.back()) {
+            b.push_back(a[i] % 10);
+            b.push_back(a[i] / 10);
+        } else {
+            b.push_back(a[i]);
+        }
+    }
+
+    cout << (is_sorted(b.rbegin(), b.rend()) ? "YES" : "NO") << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
