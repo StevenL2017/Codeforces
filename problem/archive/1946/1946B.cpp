e7b7748142aecbf769e40d7bdca7669999f4ff89
mac <>
2024-04-10 12:48:11 +0800

    update

---
+++ problem/archive/1946/1946B.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    in(a);
+
+    ll tot = 0, mx = 0, cur = 0;
+    for (auto x : a) {
+        cur = max(cur + x, x);
+        mx = max(mx, cur);
+        tot = add(tot, x);
+    }
+
+    mx %= MOD;
+    tot = sub(tot, mx);
+    while (k--) {
+        mx = add(mx, mx);
+    }
+
+    cout << add(tot, mx) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
