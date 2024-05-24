20bf3d7cc79f49e06659aa2b5372964b79c0d549
mac <>
2024-05-24 13:07:22 +0800

    update

---
+++ problem/archive/1528/1528B.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 998244353;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vi cnt(n + 1);
+    for (int i = 1; i <= n; i++) {
+        for (int j = i + i; j <= n; j += i) {
+            cnt[j]++;
+        }
+    }
+
+    int s = 1, ans = 0;
+    for (int i = 1; i <= n; i++) {
+        ans = add(s, cnt[i]);
+        s = add(s, ans);
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
