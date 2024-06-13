a1fba77f4413a3c8e89abc40ec06bb60d605bc1a
mac <>
2024-06-13 10:14:03 +0800

    update

---
+++ problem/archive/1857/1857G.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+struct Edge {
+    int u, v, w, i;
+    bool operator < (const Edge& e) const {
+        return w < e.w;
+    }
+};
+
+class UnionFind {
+public:
+    int N;
+    int *f;
+    int *sz;
+    int count;
+    
+    UnionFind(int n) :
+        N(n), count(n) {
+        f = new int[N]();
+        sz = new int[N]();
+        for (int i = 0; i < N; i++) {
+            f[i] = i;
+            sz[i] = 1;
+        }
+    }
+    
+    ~UnionFind() {
+        delete[] f;
+        delete[] sz;
+    }
+
+    int find(int x) {
+        if (x != f[x]) f[x] = find(f[x]);
+        return f[x];
+    }
+
+    bool merge(int x, int y) {
+        int x_ = find(x), y_ = find(y);
+        if (x_ == y_) return false;
+        if (sz[x_] < sz[y_]) swap(x_, y_);
+        f[y_] = x_;
+        sz[x_] += sz[y_];
+        count--;
+        return true;
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+const int MOD = 998244353;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, ll y) {
+    int z = 1;
+    while (y) {
+        if (y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+void solve() {
+    int n, s;
+    cin >> n >> s;
+    vector<Edge> e;
+    for (int i = 0; i < n - 1; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        u--; v--;
+        e.emplace_back(Edge{u, v, w, i});
+    }
+
+    sort(e.begin(), e.end());
+
+    int ans = 1;
+    UnionFind uf(n);
+    for (int i = 0; i < n - 1; i++) {
+        int u = e[i].u, v = e[i].v, w = e[i].w;
+        int c1 = uf.sz[uf.find(u)], c2 = uf.sz[uf.find(v)];
+        ll c = c1 * 1ll * c2 - 1;
+        ans = mul(ans, binpow(s - w + 1, c));
+        uf.merge(u, v);
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
