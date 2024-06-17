edfc52519c8b1cc000ef336fc828628e892c5bc0
mac <>
2024-06-17 12:34:50 +0800

    update

---
+++ problem/archive/1829/1829F.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vi> e(n);
+    vi deg(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        e[u].push_back(v);
+        e[v].push_back(u);
+        deg[u]++;
+        deg[v]++;
+    }
+
+    int leaf = 0;
+    set<int> s;
+    for (int i = 0; i < n; i++) {
+        if (deg[i] == 1) {
+            leaf++;
+            s.insert(e[i][0]);
+        }
+    }
+    cout << ssz(s) << " " << leaf / ssz(s) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
