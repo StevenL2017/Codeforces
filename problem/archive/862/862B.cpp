26507734e46b29d0c9a8b7033acaef65475fbf76
unknown <liang1058@126.com>
2024-07-07 21:28:19 +0800

    update

---
+++ problem/archive/862/862B.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vi> e(n);
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        e[u].push_back(v);
+        e[v].push_back(u);
+    }
+
+    vi cnt(2);
+    function<void(int, int, int)> dfs = [&] (int u, int p, int c) {
+        cnt[c]++;
+        for (auto v: e[u]) {
+            if (v == p) continue;
+            dfs(v, u, c ^ 1);
+        }
+    };
+    dfs(0, -1, 0);
+
+    cout << cnt[0] * 1ll * cnt[1] - (n - 1) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
