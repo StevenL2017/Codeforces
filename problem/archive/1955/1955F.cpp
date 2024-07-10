f3e310cfdf2a73e6e89e6e51864b2d33e082df88
unknown <liang1058@126.com>
2024-07-10 21:04:48 +0800

    update

---
+++ problem/archive/1955/1955F.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    vi a(4);
+    in(a);
+
+    int ans = 0;
+    if ((a[0] & 1) && (a[1] & 1) && (a[2] & 1)) ans++;
+    ans += a[0] / 2 + a[1] / 2 + a[2] / 2 + a[3] / 2;
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
