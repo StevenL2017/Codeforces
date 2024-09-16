ff29dbd84ae3ee754b4f5be062acf3e71c0f0e04
unknown <liang1058@126.com>
2024-09-16 12:22:20 +0800

    update

---
+++ problem/archive/1842/1842C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    in(a);
+
+    vi f(n + 1, n), pre(n + 1, n);
+    f[0] = 0;
+    for (int i = 0; i < n; i++) {
+        f[i + 1] = f[i] + 1;
+        f[i + 1] = min(f[i + 1], pre[a[i]]);
+        pre[a[i]] = min(pre[a[i]], f[i]);
+    }
+
+    cout << n - f[n] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
