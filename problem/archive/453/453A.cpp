2ae8ffadb292856ac69e32c1c42d9de763b8a6ad
unknown <liang1058@126.com>
2024-09-17 08:48:53 +0800

    update

---
+++ problem/archive/453/453A.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int m, n;
+    cin >> m >> n;
+
+    long double ans = m;
+    for (int i = m - 1; i > 0; i--) {
+        ans -= pow(1.0 * i / m, n);
+    }
+
+    cout.precision(10);
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
