98ba18f8708a4c88bb030a5d492a71a160376e40
unknown <liang1058@126.com>
2024-10-03 19:26:48 +0800

    1383A

---
+++ problem/archive/1383/1383A.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+class UnionFind {
+public:
+    int N;
+    int *f;
+    int *sz;
+    int count;
+    
+    UnionFind(int n) :
+        N(n), count(n) {
+        f = new int[N]();
+        sz = new int[N]();
+        for (int i = 0; i < N; i++) {
+            f[i] = i;
+            sz[i] = 1;
+        }
+    }
+    
+    ~UnionFind() {
+        delete[] f;
+        delete[] sz;
+    }
+
+    int find(int x) {
+        if (x != f[x]) f[x] = find(f[x]);
+        return f[x];
+    }
+
+    bool merge(int x, int y) {
+        int x_ = find(x), y_ = find(y);
+        if (x_ == y_) return false;
+        if (sz[x_] < sz[y_]) swap(x_, y_);
+        f[y_] = x_;
+        sz[x_] += sz[y_];
+        count--;
+        return true;
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    string a, b;
+    cin >> a >> b;
+
+    UnionFind uf(20);
+    for (int i = 0; i < n; i++) {
+        if (a[i] > b[i]) {
+            cout << -1 << endl;
+            return;
+        }
+        if (a[i] < b[i]) {
+            uf.merge(a[i] - 'a', b[i] - 'a');
+        }
+    }
+
+    int ans = 0;
+    vi vis(20);
+    for (int i = 0; i < 20; i++) {
+        if (vis[uf.find(i)]) continue;
+        ans += uf.sz[uf.find(i)] - 1;
+        vis[uf.find(i)] = 1;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
