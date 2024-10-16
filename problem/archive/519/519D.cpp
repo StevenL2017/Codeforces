5b5bcd9d46deb13beaedf8f29a6e5d4c573b6cc5
mac <>
2024-10-16 13:07:06 +0800

    519D

---
+++ problem/archive/519/519D.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    vi a(26);
+    in(a);
+    string s;
+    cin >> s;
+
+    ll ans = 0, pre = 0;
+    vector<map<ll, ll>> cnt(26);
+    for (auto c: s) {
+        ans += cnt[c - 'a'][pre];
+        pre += a[c - 'a'];
+        cnt[c - 'a'][pre]++;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
