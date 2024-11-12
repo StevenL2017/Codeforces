ffdb76cdb3278fa993a1e4d9093439245a31b5d1
mac <>
2024-11-12 12:30:51 +0800

    2004B

---
+++ problem/archive/2004/2004B.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int la, ra, lb, rb;
+    cin >> la >> ra >> lb >> rb;
+
+    int l = (la == lb ? la + 1 : max(la, lb));
+    int r = (ra == rb ? ra - 1 : min(ra, rb));
+
+    cout << max(1, r - l + 2) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
