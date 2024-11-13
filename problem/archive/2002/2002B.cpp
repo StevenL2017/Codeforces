b74ea936e961212f91110725b07d494bc36a2275
mac <>
2024-11-13 12:36:31 +0800

    2002B

---
+++ problem/archive/2002/2002B.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n), b(n);
+    in(a);
+    in(b);
+
+    if (a == b) {
+        cout << "Bob" << endl;
+        return;
+    }
+
+    reverse(b.begin(), b.end());
+
+    if (a == b) {
+        cout << "Bob" << endl;
+        return;
+    }
+
+    cout << "Alice" << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
