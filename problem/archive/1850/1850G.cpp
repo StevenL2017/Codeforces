778462bbee25ffb6f2fbb1ffa0da55124c3ed629
unknown <liang1058@126.com>
2024-11-24 00:06:08 +0800

    1850G

---
+++ problem/archive/1850/1850G.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    map<int, int> row, col, diag1, diag2;
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        ans += row[x] + col[y] + diag1[x + y] + diag2[x - y];
+        row[x]++;
+        col[y]++;
+        diag1[x + y]++;
+        diag2[x - y]++;
+    }
+    cout << ans * 2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
