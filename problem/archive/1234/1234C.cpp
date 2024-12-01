d3d116add33a4126ad4b77af6ea1edfd346a45cb
unknown <liang1058@126.com>
2024-12-01 20:35:41 +0800

    1234C

---
+++ problem/archive/1234/1234C.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<string> a(2);
+    in(a);
+
+    int i = 0, j = 0;
+    while (j < n) {
+        if (a[i][j] <= '2') {
+            j++;
+        } else if (a[i ^ 1][j] > '2') {
+            i ^= 1;
+            j++;
+        } else {
+            break;
+        }
+    }
+    cout << (i == 1 && j == n ? "YES" : "NO") << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
