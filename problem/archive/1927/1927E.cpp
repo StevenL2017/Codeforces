825df79e1890a9d26198553668888c274464c6f9
unknown <liang1058@126.com>
2024-12-07 09:53:42 +0800

    1927E

---
+++ problem/archive/1927/1927E.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    vi ans(n);
+    int s = 1, e = n;
+    for (int i = 0; i < k; i++) {
+        for (int j = i; j < n; j += k) {
+            if (i % 2 == 0) {
+                ans[j] = s++;
+            } else {
+                ans[j] = e--;
+            }
+        }
+    }
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
