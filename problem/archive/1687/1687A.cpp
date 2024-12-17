fbf454fb6a10bd3d8cd7f74acd37f820f371cdee
mac <>
2024-12-17 12:36:32 +0800

    1687A

---
+++ problem/archive/1687/1687A.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    in(a);
+
+    vector<ll> pre(n + 1);
+    for (int i = 0; i < n; i++) {
+        pre[i + 1] = pre[i] + a[i];
+    }
+
+    if (k >= n) {
+        cout << pre[n] + (k - n) * 1ll * n + (n - 1) * 1ll * n / 2 << endl;
+    } else {
+        ll mx = pre[k];
+        for (int i = k + 1; i <= n; i++) {
+            mx = max(mx, pre[i] - pre[i - k]);
+        }
+        cout << mx + (k - 1) * 1ll * k / 2 << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
