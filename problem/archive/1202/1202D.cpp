94a6492a82f5d6be65fecb19a0f5a938c462a68e
mac <>
2025-01-02 12:41:06 +0800

    1202D

---
+++ problem/archive/1202/1202D.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int m = (int)(sqrt(2 * n));
+    while ((m + 1) * m <= 2 * n) {
+        m++;
+    }
+    int r = n - m * (m - 1) / 2;
+
+    string ans = "133";
+    while (r) {
+        ans += '7';
+        r--;
+    }
+    m -= 2;
+    while (m) {
+        ans += '3';
+        m--;
+    }
+    ans += '7';
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
