b7616f1203d8b6125a75658281c67d1f87e9ff1f
unknown <liang1058@126.com>
2025-01-12 09:18:38 +0800

    2007C

---
+++ problem/archive/2007/2007C.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, x, y;
+    cin >> n >> x >> y;
+    vi a(n);
+    in(a);
+
+    int d = gcd(x, y);
+    for (auto &c: a) c %= d;
+    sort(begin(a), end(a));
+
+    int ans = a.back() - a.front();
+    for (int i = 1; i < n; i++) {
+        ans = min(ans, a[i - 1] + d - a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
