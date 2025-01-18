2ef55b1a511a6cfce512de75b1051ecd2d3ad994
mac <>
2025-01-18 12:54:41 +0800

    1840D

---
+++ problem/archive/1840/1840D.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    in(a);
+
+    sort(begin(a), end(a));
+
+    auto check = [&] (int x) -> bool {
+        int cnt = 1, pre = a[0];
+        for (int i = 1; i < n; i++) {
+            if (a[i] - pre > x * 2) {
+                cnt++;
+                pre = a[i];
+            }
+        }
+        return cnt <= 3;
+    };
+
+    int left = 0, right = 1e9;
+    int ans = right;
+    while (left <= right) {
+        auto mid = left + (right - left) / 2;
+        if (check(mid)) {
+            right = mid - 1;
+            ans = mid;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
