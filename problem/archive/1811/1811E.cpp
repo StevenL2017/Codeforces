4ab7b50bf4ebe188d86b50d81e8c537bc70c68e0
unknown <liang1058@126.com>
2025-01-20 22:59:17 +0800

    1811E

---
+++ problem/archive/1811/1811E.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    vi a;
+    while (n) {
+        a.push_back(n % 9);
+        n /= 9;
+    }
+
+    reverse(begin(a), end(a));
+
+    for (auto x: a) {
+        if (x >= 4) {
+            cout << x + 1;
+        } else {
+            cout << x;
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
