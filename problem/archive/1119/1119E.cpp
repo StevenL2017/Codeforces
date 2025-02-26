1771f1d65abe1b1a3a7fe2d4a9ac65ec70a37207
StevenL2017 <liang1058@126.com>
2025-02-27 00:15:04 +0800

    1119E

---
+++ problem/archive/1119/1119E.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    in(a);
+
+    ll ans = 0, rem = 0;
+    for (int i = 0; i < n; i++) {
+        auto d = min(rem, a[i] / 2);
+        ans += d;
+        rem -= d;
+        a[i] -= d * 2;
+        d = a[i] / 3;
+        ans += d;
+        rem += a[i] - d * 3;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
