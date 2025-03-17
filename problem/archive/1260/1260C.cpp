5c1849fccae069a11e1c78295238fca53c9578a4
StevenL2017 <liang1058@126.com>
2025-03-18 00:33:51 +0800

    1260C

---
+++ problem/archive/1260/1260C.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll r, b, k;
+    cin >> r >> b >> k;
+
+    if (r > b) swap(r, b);
+    auto g = gcd(r, b);
+    cout << ((b > (k - 1) * r + g) ? "REBEL" : "OBEY") << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
