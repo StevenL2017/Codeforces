502895dbfeb8b69ef09dbcabdfd07538e9fd5a2c
mac <>
2025-03-24 19:39:52 +0800

    1985G

---
+++ problem/archive/1985/1985G.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+int binpow(int x, int y) {
+    int z = 1;
+    while (y) {
+        if (y & 1) z = mul(z, x);
+        x = mul(x, x);
+        y >>= 1;
+    }
+    return z;
+}
+
+void solve() {
+    int l, r, k;
+    cin >> l >> r >> k;
+
+    cout << sub(binpow(9 / k + 1, r), binpow(9 / k + 1, l)) << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
