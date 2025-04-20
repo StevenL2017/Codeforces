0fd03b2a1d8017c7940979970b99150a8b0e9a8e
StevenL2017 <liang1058@126.com>
2025-04-20 23:26:04 +0800

    982C

---
+++ problem/archive/982/982C.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vi> e(n);
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        e[u].push_back(v);
+        e[v].push_back(u);
+    }
+
+    if (n & 1) {
+        cout << -1 << endl;
+        return;
+    }
+
+    int ans = 0;
+    function<int(int, int)> dfs = [&] (int u, int p) -> int {
+        int sz = 1;
+        for (auto v: e[u]) {
+            if (v == p) {
+                continue;
+            }
+            int nxt = dfs(v, u);
+            if (nxt & 1) {
+                sz += nxt;
+            } else {
+                ans++;
+            }
+        }
+        return sz;
+    };
+    dfs(0, -1);
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
