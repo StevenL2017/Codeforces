c088071e7fa5638c6f22f5eef465b7f82178ac4a
mac <>
2025-04-28 20:38:34 +0800

    678C

---
+++ problem/archive/678/678C.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll n, a, b, p, q;
+    cin >> n >> a >> b >> p >> q;
+
+    ll x = lcm(a, b);
+    ll ca = n / a, cb = n / b, cx = n / x;
+    ll ans = (ca - cx) * p + (cb - cx) * q + cx * max(p, q);
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
