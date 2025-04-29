660e28e461a7b3cde856d3e8939a0648168dc146
mac <>
2025-04-29 23:11:19 +0800

    739A

---
+++ problem/archive/739/739A.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    int mn = n;
+    for (int i = 0; i < m; i++) {
+        int l, r;
+        cin >> l >> r;
+        mn = min(mn, r - l + 1);
+    }
+
+    vi ans(n);
+    int cur = 0;
+    for (int i = 0; i < n; i++) {
+        ans[i] = cur;
+        cur = (cur + 1) % mn;
+    }
+
+    cout << mn << endl;
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
