466a2bec9ea5febc12d9aa8c55c1c79dfa5b4e16
unknown <liang1058@126.com>
2025-05-06 00:25:32 +0800

    582A

---
+++ problem/archive/582/582A.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    multiset<int> s;
+    for (int i = 0; i < n * n; i++) {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+
+    vi ans;
+    while (!s.empty()) {
+        int cur = *s.rbegin();
+        s.extract(cur);
+        for (auto x: ans) {
+            s.extract(gcd(cur, x));
+            s.extract(gcd(cur, x));
+        }
+        ans.push_back(cur);
+    }
+    out(ans);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
