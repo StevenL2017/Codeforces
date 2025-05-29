d69f3d86552884a064900df1272600766af2718d
unknown <liang1058@126.com>
2025-05-29 22:53:35 +0800

    1929D

---
+++ problem/archive/1929/1929D.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 998244353;
+
+int add(int x, int y) {
+    x += y;
+    while (x >= MOD) x -= MOD;
+    while (x < 0) x += MOD;
+    return x;
+}
+
+int sub(int x, int y) {
+    return add(x, -y);
+}
+
+int mul(int x, int y) {
+    return (x * 1ll * y) % MOD;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<vi> e(n);
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        e[u].push_back(v);
+        e[v].push_back(u);
+    }
+
+    int ans = 1;
+    function<int(int, int)> dfs = [&] (int u, int p) -> int {
+        int cur = 1;
+        for (auto v: e[u]) {
+            if (v == p) {
+                continue;
+            }
+            int nxt = dfs(v, u);
+            cur = mul(cur, add(nxt, 1));
+        }
+        ans = add(ans, cur);
+        return cur;
+    };
+    dfs(0, -1);
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
