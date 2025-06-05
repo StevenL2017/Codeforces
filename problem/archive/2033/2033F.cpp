c402d9482fc62dc612640817b7ee649e07549e8e
unknown <liang1058@126.com>
2025-06-05 22:40:01 +0800

    2033F

---
+++ problem/archive/2033/2033F.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+const int MOD = 1e9 + 7;
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+
+    int a = 0, b = 1 % k, cnt = 0;
+    do {
+        cnt++;
+        int c = (a + b) % k;
+        a = b;
+        b = c;
+    } while (a);
+    cout << cnt * (n % MOD) % MOD << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
