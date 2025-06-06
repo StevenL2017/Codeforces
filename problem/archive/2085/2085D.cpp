9ceba74dce6a618ead8049a5f07a49bba3cd8456
mac <>
2025-06-06 18:20:50 +0800

    2085D

---
+++ problem/archive/2085/2085D.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    in(a);
+
+    ll ans = 0;
+    multiset<int> s;
+    for (int i = 1; i <= n; i++) {
+        s.insert(a[i - 1]);
+        if ((n - i + 1) % (k + 1) == 0) {
+            int mx = *s.rbegin();
+            ans += mx;
+            s.erase(s.find(mx));
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
