8517e0f3f523864cb6f98aad60c521b05306fee2
unknown <liang1058@126.com>
2025-06-07 17:36:13 +0800

    2032C

---
+++ problem/archive/2032/2032C.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    in(a);
+
+    sort(begin(a), end(a));
+
+    int ans = 0;
+    for (int i = 2, j = 0; i < n; i++) {
+        while (j + 2 <= i && a[j] + a[j + 1] <= a[i]) {
+            j++;
+        }
+        ans = max(ans, i - j + 1);
+    }
+    cout << n - ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
