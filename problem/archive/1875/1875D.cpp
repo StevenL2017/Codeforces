675b162b35c62e041b8542d26d1b3dc95ed40b84
mac <>
2025-06-17 19:11:11 +0800

    1875D

---
+++ problem/archive/1875/1875D.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    int n;
+    cin >> n;
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        cnt[x]++;
+    }
+
+    int mex = 0;
+    while (cnt[mex]) {
+        mex++;
+    }
+
+    vector<ll> f(mex + 1, 1e18);
+    f[mex] = 0;
+    for (int i = mex; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
+            f[j] = min(f[j], f[i] + cnt[j] * 1ll * i);
+        }
+    }
+    cout << f[0] - mex << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
