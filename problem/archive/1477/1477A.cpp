9d632d60a1686e2e8cf1515e9cf7ee4e34c87b28
unknown <liang1058@126.com>
2025-06-19 22:01:42 +0800

    1477A

---
+++ problem/archive/1477/1477A.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdio>
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <bitset>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define ssz(a) (int)((a).size())
+
+#define endl '\n'
+template <class T> void in(vector<T>& a) { for (int i = 0; i < ssz(a); i++) cin >> a[i]; }
+template <class T> void out(const vector<T>& a) { for (int i = 0; i < ssz(a); i++) cout << a[i] << " \n"[i + 1 == ssz(a)]; }
+
+void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    in(a);
+
+    ll mn = *min_element(begin(a), end(a)), g = 0;
+    for (auto x: a) {
+        g = gcd(g, x - mn);
+    }
+    cout << ((k - mn) % g == 0 ? "YES" : "NO") << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+}
